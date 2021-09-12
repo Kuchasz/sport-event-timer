197dbcb61188b39a3c6dbd5d9ce8b364e16ccc2c
Rafal Pysz <kuchasz1@o2.pl>
2021-09-12 19:21:24 +0200

    test

--- mobile/src/index.tsx
+++ mobile/src/index.tsx
@@ -3,26 +3,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { AppDispatch, createStore, RootState } from "@set/timer/store";
-import { io } from "socket.io-client";
 import { Middleware } from "redux";
 import { Provider } from "react-redux";
 import "./index.scss";
+// import { io } from "socket.io-client";
 
-const socket = io("ws://wss.set-hub.pyszstudio.pl", { transports: ["websocket"] });
-socket.on("connect", () => {
-    console.log(socket.id);
-});
+// const socket = io("wss://wss.set-hub.pyszstudio.pl", { transports: ["websocket"] });
+// socket.on("connect", () => {
+//     console.log(socket.id);
+// });
 
-socket.on("disconnect", (r) => {
-    console.log("DISCONNECTED", r);
-});
+// socket.on("disconnect", (r) => {
+//     console.log("DISCONNECTED", r);
+// });
 
-socket.on("receive-action", (action) => store.dispatch({ ...action, __remote: true }));
+// socket.on("receive-action", (action) => store.dispatch({ ...action, __remote: true }));
 
 export const exampleMiddleware: Middleware<{}, RootState, AppDispatch> = (storeApi) => (next) => (action) => {
     // const state = storeApi.getState();
     console.log(action);
-    if (!action.__remote && socket.connected) socket.emit("post-action", action);
+    // if (!action.__remote && socket.connected) socket.emit("post-action", action);
 
     next(action);
 };
