a1754f2000fe51fccf501e3e82a34f5d76c18b4a
Rafal Pysz <kuchasz1@o2.pl>
2021-09-14 00:14:20 +0200

    improved connection state bar

--- mobile/src/components/status.tsx
+++ mobile/src/components/status.tsx
@@ -1,6 +1,29 @@
+import Icon from "@mdi/react";
 import { ConnectionState, onConnectionStateChanged } from "../connection";
+import {
+    mdiWifi,
+    mdiWifiOff,
+    mdiWifiStrengthAlertOutline,
+    mdiWifiSync
+    } from "@mdi/js";
 import { Timer } from "./timer";
 import { useEffect, useState } from "react";
+
+const getIconFromConnectionState = (state: ConnectionState) => {
+    switch (state) {
+        case "connected":
+            return mdiWifi;
+        case "disconnected":
+            return mdiWifiOff;
+        case "error":
+            return mdiWifiStrengthAlertOutline;
+        case "reconnecting":
+            return mdiWifiSync;
+        default:
+            throw new Error("not handled connection state");
+    }
+};
+
 type StatusProps = { timeKeeperName: string };
 export const Status = ({ timeKeeperName }: StatusProps) => {
     const [connectionState, setConnectionState] = useState<ConnectionState>("disconnected");
@@ -12,9 +35,9 @@ export const Status = ({ timeKeeperName }: StatusProps) => {
 
     return (
         <div className="px-5 w-screen flex-shrink-0 flex items-center justify-between bg-white font-semibold text-black border h-10">
-            <span className="mr-4">{timeKeeperName}</span>
-            {connectionState}
+            <span>{timeKeeperName}</span>
             <Timer />
+            <Icon path={getIconFromConnectionState(connectionState)} size={1} />
         </div>
     );
 };
