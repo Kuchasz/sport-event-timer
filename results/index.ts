aeeed3b21a365e30b4b15a0a338b105ecc1b2c42
Rafal Pysz <kuchasz1@o2.pl>
2021-09-19 11:54:14 +0200

    changing path to next dir in prod

--- results/index.ts
+++ results/index.ts
@@ -3,7 +3,7 @@ import { Express } from "express";
 import { resolve } from "path";
 
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev, dir: resolve(__dirname) });
+const app = next({ dev, dir: dev ? resolve(__dirname) : resolve(__dirname + "../") });
 const handle = app.getRequestHandler();
 
 export const apply = async (server: Express): Promise<void> => {
