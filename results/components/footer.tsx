f3f242b927f0f8aef7f71c9420de6f8e50a3a6f8
Rafal Pysz <kuchasz1@o2.pl>
2021-11-09 00:27:00 +0200

    extended footer + slogan

--- results/components/footer.tsx
+++ results/components/footer.tsx
@@ -1,5 +1,8 @@
 import classNames from "classnames";
+import Icon from "@mdi/react";
 import Link from "next/link";
+import { Email } from "./email";
+import { mdiEmailOpenOutline, mdiHumanMaleChild } from "@mdi/js";
 import { menuItems } from "./menu-items";
 import { ReactNode } from "react";
 import { useRouter } from "next/dist/client/router";
@@ -19,15 +22,45 @@ const MenuButton = ({ activePath = "", to, children }: { activePath: string; to:
 const Footer = () => {
     const router = useRouter();
     return (
-        <footer className="bg-gray-800 text-gray-600 flex justify-center py-12 text-xs font-semibold">
-            <div className="w-full max-w-5xl flex justify-between">
-                <div>RURA NA KOCIERZ © 2021</div>
-                <div>
-                    {menuItems.map((mi) => (
-                        <MenuButton key={mi.path} activePath={router.asPath} to={mi.path}>
-                            {mi.label}
-                        </MenuButton>
-                    ))}
+        <footer>
+            <div className="flex justify-center py-8 bg-gray-900 text-white">
+                <div className="w-full max-w-5xl flex text-sm items-center">
+                    <img className="mr-10" width="150px" src="assets/logo-sm.png"></img>
+                    <div className="flex-grow text-gray-400">
+                        <div className="text-lg">
+                            Ambasador <strong>Marta Lach</strong>
+                        </div>
+                        <div>Mistrzyni Polski, Olimpijka Tokio 2020.</div>
+                    </div>
+                    <div className="flex items-center">
+                        <div className="flex">
+                            <Icon className="text-orange-500" size={1.5} path={mdiHumanMaleChild}></Icon>
+                            <div className="ml-4">
+                                <div className="text-gray-700 font-bold">ZAWODY DLA DZIECI</div>
+                                <Email>zawodydzieci@rura.cc</Email>
+                            </div>
+                        </div>
+                        <div className="h-1/2 mx-12 w-0.5 bg-gray-700 py-5"></div>
+                        <div className="flex">
+                            <Icon className="text-orange-500" size={1.5} path={mdiEmailOpenOutline}></Icon>
+                            <div className="ml-4">
+                                <div className="text-gray-700 font-bold">KONTAKT</div>
+                                <Email>biuro@rura.cc</Email>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-gray-800 text-gray-600 flex justify-center py-12 text-xs font-semibold">
+                <div className="w-full max-w-5xl flex justify-between">
+                    <div>RURA NA KOCIERZ © 2021</div>
+                    <div>
+                        {menuItems.map((mi) => (
+                            <MenuButton key={mi.path} activePath={router.asPath} to={mi.path}>
+                                {mi.label}
+                            </MenuButton>
+                        ))}
+                    </div>
                 </div>
             </div>
         </footer>
--- results/pages/index.tsx
+++ results/pages/index.tsx
@@ -2,7 +2,21 @@ import Head from "next/head";
 import { TimerState } from "@set/timer/store";
 
 const Slogan = () => (
-    <div style={{ backgroundImage: "url('assets/kocierz.jpg')" }} className="w-full h-128 bg-bottom bg-cover"></div>
+    <div
+        style={{ backgroundImage: "url('assets/kocierz.jpg')" }}
+        className="flex w-full h-128 text-white bg-bottom bg-cover justify-center"
+    >
+        <div className="w-full max-w-5xl flex flex-col items-start justify-center">
+            <div className="text-5xl uppercase font-bold">Rura na Kocierz 2022</div>
+            <div className="py-4">Ambasador Marta Lach. Mistrzyni Polski, Olimpijka Tokio 2020.</div>
+            <div className="text-xl drop-shadow-xl">
+                <strong>09.04.2022</strong> Time Trial
+            </div>
+            <div className="text-xl drop-shadow-xl">
+                <strong>10.04.2022</strong> Wyścig ze startu wspólnego
+            </div>
+        </div>
+    </div>
 );
 
 type Props = {
@@ -16,9 +30,7 @@ const Index = ({}: Props) => {
                 <title>Rura na Kocierz</title>
             </Head>
 
-            <div>
-                <Slogan />
-            </div>
+            <Slogan />
         </>
     );
 };
