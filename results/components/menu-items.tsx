ebad092cf3afb6b35cc731b6049936f21106bb1b
Rafal Pysz <kuchasz1@o2.pl>
2021-11-27 01:40:25 +0200

    hiding some menu options

--- results/components/menu-items.tsx
+++ results/components/menu-items.tsx
@@ -1,9 +1,9 @@
 export const menuItems = [
-    { path: "/", label: "home" },
-    { path: "/zegar", label: "zegar" },
+    { path: "/", label: "aktualności" },
+    // { path: "/zegar", label: "zegar" },
     { path: "/regulamin", label: "regulamin" },
-    { path: "/lista", label: "lista startowa" },
-    { path: "/live", label: "na żywo" },
-    { path: "/wyniki", label: "wyniki" },
+    // { path: "/lista", label: "lista startowa" },
+    // { path: "/live", label: "na żywo" },
+    // { path: "/wyniki", label: "wyniki" },
     { path: "/zdjecia", label: "zdjęcia" }
 ];
