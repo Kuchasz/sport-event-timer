f5c6d6814352625d629d748ddb6a7a37387f8767
Rafal Pysz <kuchasz1@o2.pl>
2021-11-28 16:41:38 +0100

    drop_shadow tweak

--- results/tailwind.config.js
+++ results/tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
                 "-1": "-1"
             },
             dropShadow: {
-                "3xl": "0 25px 25px rgba(0, 0, 0, 1)",
+                "3xl": "0 3px 5px black",
                 "w-2xl": "0 2px 10px rgba(255, 255, 255, 0.2)"
             },
             gridTemplateColumns: {
