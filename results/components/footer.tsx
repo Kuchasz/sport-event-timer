4e29ea60dabcd23463d5ccbe54bfa52db023cfea
Rafal Pysz <kuchasz1@o2.pl>
2022-01-23 14:04:47 +0100

    partners logos added

--- results/components/footer.tsx
+++ results/components/footer.tsx
@@ -35,6 +35,63 @@ const Footer = () => {
     const router = useRouter();
     return (
         <footer>
+            <div className="flex flex-col items-center py-8 bg-gray-100 text-white">
+                <div className="text-gray-700 font-semibold text-2xl">NASI PARTNERZY</div>
+                <div className="h-1 w-10 bg-orange-500 my-6"></div>
+                <div className="max-w-6xl flex flex-wrap justify-center items-center">
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-agropunkt.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-ceratizit.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-czaniecki-makaron.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-decathlon.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-euro-stempel.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-eurowafel.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-gmina-lekawica.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-gmina-porabka.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-kocierz.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-kolarsko-pl.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-maspex.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-pbw.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-pkp-cargo-service.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-power-of-science.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-ravelo.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-trek.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-turdetur.png"></img>
+                    <img className="-m-6 scale-50" src="/assets/partners/logo-velonews-pl.png"></img>
+                    {/* 
+                    <img width="200" height="200" className="p-4" src="/assets/partners/logo1.png"></img>
+                    <img width="200" height="200" className="p-4" src="/assets/partners/logo1.png"></img>
+                    <img width="200" height="200" className="p-4" src="/assets/partners/logo1.png"></img>
+                    <img width="200" height="200" className="p-4" src="/assets/partners/logo1.png"></img>
+                    <img width="200" height="200" className="p-4" src="/assets/partners/logo1.png"></img>
+                    <img width="200" height="200" className="p-4" src="/assets/partners/logo1.png"></img> */}
+                </div>
+                {/* <div className="w-full max-w-6xl flex flex-col sm:flex-row text-sm items-center">
+                    <img className="mr-10" width="150px" src="/assets/logo-sm.png"></img>
+                    <div className="flex-grow text-gray-400">
+                        <div className="text-lg">
+                            Ambasador <strong>Marta Lach</strong>
+                        </div>
+                        <div>Mistrzyni Polski, Olimpijka Tokio 2020</div>
+                    </div>
+                    <div className="flex flex-col sm:flex-row  items-center">
+                        <div className="flex">
+                            <Icon className="text-orange-500" size={1.5} path={mdiHumanMaleChild}></Icon>
+                            <div className="ml-4">
+                                <div className="text-gray-700 font-bold">ZAWODY DLA DZIECI</div>
+                                <Email>zawodydzieci@rura.cc</Email>
+                            </div>
+                        </div>
+                        <div className="h-1/2 mx-12 w-0.5 bg-gray-700 py-5"></div>
+                        <div className="flex">
+                            <Icon className="text-orange-500" size={1.5} path={mdiEmailOpenOutline}></Icon>
+                            <div className="ml-4">
+                                <div className="text-gray-700 font-bold">KONTAKT</div>
+                                <Email>biuro@rura.cc</Email>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+            </div>
             <div className="flex justify-center py-8 bg-gray-900 text-white">
                 <div className="w-full max-w-6xl flex flex-col sm:flex-row text-sm items-center">
                     <img className="mr-10" width="150px" src="/assets/logo-sm.png"></img>
