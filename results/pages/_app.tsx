6dc5e8bf1ce194e5eb2915e2ce9c330024a4ad68
Rafal Pysz <kuchasz1@o2.pl>
2022-01-23 17:35:51 +0100

    galleries tweaks

--- results/pages/_app.tsx
+++ results/pages/_app.tsx
@@ -1,14 +1,14 @@
 import Layout from "../components/layout";
+import SimpleReactLightbox from "simple-react-lightbox";
 import { AppProps } from "next/app";
 import "../globals.scss";
-import "lightgallery/css/lightgallery.css";
-import "lightgallery/css/lg-zoom.css";
-import "lightgallery/css/lg-thumbnail.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <Layout>
-            <Component {...pageProps} />
+            <SimpleReactLightbox>
+                <Component {...pageProps} />
+            </SimpleReactLightbox>
         </Layout>
     );
 }
