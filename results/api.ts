3e9de56082151055a8a3f591a7e035d8356c284d
Rafal Pysz <kuchasz1@o2.pl>
2022-03-13 02:51:15 +0100

    ntp sync from stackoverflow

--- results/api.ts
+++ results/api.ts
@@ -7,11 +7,13 @@ export const getPlayers = (): Promise<Player[]> => fetch(`${hubUrl}/players`).th
 export const getPlayersDate = (): Promise<number> => fetch(`${hubUrl}/players-date`).then((x) => x.json());
 export const getCurrentTimeOffset = (): Promise<number> => {
     return new Promise<number>(async (res) => {
-        const t0 = Date.now();
-        const { t1 } = await fetch(`${hubUrl}/current-time/${t0}`).then((x) => x.json());
+        const clientTime = Date.now();
+        const reqSendTime = Date.now();
+        const { serverTime } = await fetch(`${hubUrl}/current-time`).then((x) => x.json());
+        const reqBackTime = Date.now();
 
-        const t2 = Date.now();
+        const delay = (reqBackTime - reqSendTime) / 2;
 
-        res(t2 - t1 + (t2 - t0) / 2);
+        res(clientTime - (serverTime + delay));
     });
 };
