d446f616ef7566e81c546817426fccd94e041b7b
Rafal Pysz <kuchasz1@o2.pl>
2022-03-21 00:23:32 +0100

    build.tweak.5

--- mobile/craco.config.js
+++ mobile/craco.config.js
@@ -1,34 +1,34 @@
-const path = require("path");
-const { getLoader, loaderByName } = require("@craco/craco");
-const timerModulePath = path.join(__dirname, "../timer");
+// const path = require("path");
+// const { getLoader, loaderByName } = require("@craco/craco");
+// const timerModulePath = path.join(__dirname, "../timer");
 
-module.exports = {
-    style: {
-        postcss: {
-            plugins: [require("tailwindcss"), require("autoprefixer")]
-        }
-    },
-    webpack: {
-        alias: {},
-        plugins: [],
-        configure: (webpackConfig, { env, paths }) => {
-            const { isFound, match } = getLoader(webpackConfig, loaderByName("babel-loader"));
-            if (isFound) {
-                const include = Array.isArray(match.loader.include) ? match.loader.include : [match.loader.include];
-                match.loader.include = include.concat[timerModulePath];
-            }
+// module.exports = {
+//     style: {
+//         postcss: {
+//             plugins: [require("tailwindcss"), require("autoprefixer")]
+//         }
+//     },
+//     webpack: {
+//         alias: {},
+//         plugins: [],
+//         configure: (webpackConfig, { env, paths }) => {
+//             const { isFound, match } = getLoader(webpackConfig, loaderByName("babel-loader"));
+//             if (isFound) {
+//                 const include = Array.isArray(match.loader.include) ? match.loader.include : [match.loader.include];
+//                 match.loader.include = include.concat[timerModulePath];
+//             }
 
-            webpackConfig.module.rules.concat({
-                test: /\.js$/,
-                loader: "webpack-remove-debug" // remove "debug" package
-            });
+//             webpackConfig.module.rules.concat({
+//                 test: /\.js$/,
+//                 loader: "webpack-remove-debug" // remove "debug" package
+//             });
 
-            const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
-                ({ constructor }) => constructor && constructor.name === "ModuleScopePlugin"
-            );
+//             const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+//                 ({ constructor }) => constructor && constructor.name === "ModuleScopePlugin"
+//             );
 
-            webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
-            return webpackConfig;
-        }
-    }
-};
+//             webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
+//             return webpackConfig;
+//         }
+//     }
+// };
