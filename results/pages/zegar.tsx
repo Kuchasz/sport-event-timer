c5d2ef3f73efceaaf3622367754823a4c1a67649
Rafal Pysz <kuchasz1@o2.pl>
2022-03-20 01:08:36 +0100

    settings for clock

--- results/pages/zegar.tsx
+++ results/pages/zegar.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
+import Icon from "@mdi/react";
 import React from "react";
 import { Countdown } from "components/countdown";
 import { Loader } from "../components/loader";
+import {
+    mdiCog,
+    mdiEyeOffOutline,
+    mdiFormatFontSizeDecrease,
+    mdiFormatFontSizeIncrease,
+    mdiWindowClose
+    } from "@mdi/js";
 import { socket } from "../connection";
 import { Timer } from "../components/timer";
 import { useEffect, useState } from "react";
@@ -9,14 +17,92 @@ import { useEffect, useState } from "react";
 // import { create } from "timesync";
 // import { getCurrentTimeOffset, timeSyncUrl } from '../api';
 
+type TextSettings = {
+    enabled: boolean;
+    size: number;
+};
+
+type ClockSettings = {
+    showSettings: boolean;
+    clock: TextSettings;
+    countdown: TextSettings;
+    currentPlayer: TextSettings;
+    nextPlayer: TextSettings;
+};
+
+type TextActions = {
+    enlargeFont: () => void;
+    minifyFont: () => void;
+    toggle: () => void;
+};
+
+const defaultClockSettings: ClockSettings = {
+    showSettings: false,
+    clock: { enabled: true, size: 6 },
+    countdown: { enabled: true, size: 90 },
+    currentPlayer: { enabled: true, size: 6 },
+    nextPlayer: { enabled: true, size: 3 }
+};
+
+const ConfigButton = ({ text, path, click }: { text: string; path: string; click: () => void }) => (
+    <span className="flex items-center py-2 cursor-pointer" onClick={click}>
+        <div className="bg-orange-500 p-2 rounded-full text-white">
+            <Icon size={1} path={path} />
+        </div>
+        <span className="ml-2">{text}</span>
+    </span>
+);
+
+const ConfigMenuOption = ({
+    actions,
+    name,
+    showDivider
+}: {
+    actions: TextActions;
+    name: string;
+    showDivider: boolean;
+}) => (
+    <div>
+        {/* <div>{name}</div> */}
+        <div className="flex flex-col p-2">
+            {showDivider && (
+                <div className="relative flex py-5 items-center">
+                    <div className="flex-grow border-t"></div>
+                </div>
+            )}
+            <span>{name}</span>
+            <ConfigButton click={actions.toggle} path={mdiEyeOffOutline} text="Ukryj" />
+            <ConfigButton click={actions.enlargeFont} path={mdiFormatFontSizeIncrease} text="Powiększenie czcionki" />
+            <ConfigButton click={actions.minifyFont} path={mdiFormatFontSizeDecrease} text="Pomniejszenie czcionki" />
+        </div>
+    </div>
+);
+
 const Zegar = () => {
     const [timeOffset, setTimeOffset] = useState<number>();
+    const [clockState, setClockState] = useState<ClockSettings>(defaultClockSettings);
+
+    const textActions = (prop: keyof Omit<ClockSettings, "showSettings">, step: number = 1): TextActions => ({
+        enlargeFont: () => {
+            const textState = clockState[prop];
+            setClockState({ ...clockState, [prop]: { ...textState, size: textState.size + step } });
+        },
+        minifyFont: () => {
+            const textState = clockState[prop];
+            setClockState({ ...clockState, [prop]: { ...textState, size: textState.size - step } });
+        },
+        toggle: () => {
+            const textState = clockState[prop];
+            setClockState({ ...clockState, [prop]: { ...textState, enabled: !textState.enabled } });
+        }
+    });
+
+    const toggleMenu = () => {
+        setClockState({ ...clockState, showSettings: !clockState.showSettings });
+    };
+
     useEffect(() => {
         // const req = new XMLHttpRequest();
-
-        // for (var i = 0; i < 100; i++) {
-        //     fetch("https://jsonplaceholder.typicode.com/todos/1");
-        // }
 
         // req.onreadystatechange = (e) => {
         //     console.log(req.readyState);
@@ -40,6 +126,7 @@ const Zegar = () => {
 
         // req.open("GET", timeSyncUrl);
         // req.send();
+
         let loadStartTime = Date.now();
         socket.on("TR", (serverTime) => {
             const loadEndTime = Date.now();
@@ -57,40 +144,6 @@ const Zegar = () => {
             loadStartTime = Date.now();
             socket.emit("TQ");
         }, 1000);
-
-        // console.log("create!");
-        // const ts = create({ server: timeSyncUrl, interval: 5000, delay: 5000 });
-
-        // let previousTimeOffset = Date.now();
-        // ts.sync();
-        // let synced = false;
-
-        // ts.on("sync", (e) => {
-        //     if (e === "end") {
-        //         const offset = Math.abs(Date.now() - ts.now());
-        //         if (offset < previousTimeOffset) {
-        //             console.log(offset);
-        //             previousTimeOffset = offset;
-        //             setTimeOffset(offset);
-        //         }
-
-        //         if (offset < 100) {
-        //             console.log("destroy!");
-        //             ts.destroy();
-        //         }
-
-        //         // synced = true;
-        //     }
-        // });
-
-        // setInterval(() => {
-        //     setTimeOffset(Date.now() - ts.now());
-        // }, 5000);
-
-        // setInterval(() => setTimeOffset(ts.now()), 5000);
-        // console.log();
-        // getCurrentTimeOffset().then(setTimeOffset);
-        // setTimeout(() => getCurrentTimeOffset().then(setTimeOffset), 5000);
     }, []);
     return (
         <>
@@ -105,10 +158,62 @@ const Zegar = () => {
                     </div>
                 ) : (
                     <div className="w-full h-full flex flex-col items-center">
-                        <Timer offset={timeOffset} />
-                        <Countdown offset={timeOffset} />
-                        <div className="text-8xl">John Doe</div>
-                        <div className="text-4xl mb-4">Następny: John Doe</div>
+                        {clockState.clock.enabled && <Timer fontSize={clockState.clock.size} offset={timeOffset} />}
+                        {clockState.countdown.enabled && (
+                            <Countdown fontSize={clockState.countdown.size} offset={timeOffset} />
+                        )}
+                        {clockState.currentPlayer.enabled && (
+                            <div style={{ fontSize: `${clockState.currentPlayer.size}rem` }} className="transition-all">
+                                John Doe
+                            </div>
+                        )}
+                        {clockState.nextPlayer.enabled && (
+                            <div
+                                style={{ fontSize: `${clockState.nextPlayer.size}rem` }}
+                                className="mb-4 transition-all"
+                            >
+                                Następny: John Doe
+                            </div>
+                        )}
+
+                        {clockState.showSettings ? (
+                            <div className="absolute left-0 top-0 h-full select-none">
+                                <div className="w-80 bg-zinc-100 h-full text-zinc-600 overflow-y-auto">
+                                    <div className="flex p-2 items-center justify-between bg-orange-500 text-white text-2xl mb-4 font-medium">
+                                        Ustawienia
+                                        <div onClick={toggleMenu}>
+                                            <Icon className="cursor-pointer m-2" size={1} path={mdiWindowClose} />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <ConfigMenuOption
+                                            actions={textActions("clock")}
+                                            showDivider={false}
+                                            name="Zegar"
+                                        />
+                                        <ConfigMenuOption
+                                            actions={textActions("countdown", 6)}
+                                            showDivider={true}
+                                            name="Stoper"
+                                        />
+                                        <ConfigMenuOption
+                                            actions={textActions("currentPlayer")}
+                                            showDivider={true}
+                                            name="Aktualny zawodnik"
+                                        />
+                                        <ConfigMenuOption
+                                            actions={textActions("nextPlayer")}
+                                            showDivider={true}
+                                            name="Następny zawodnik"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        ) : (
+                            <div onClick={toggleMenu} className="absolute top-0 left-0 p-4">
+                                <Icon size={2} path={mdiCog} />
+                            </div>
+                        )}
                     </div>
                 )}
             </div>
