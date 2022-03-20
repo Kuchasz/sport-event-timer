8c691b1212b9d1a943b81521f61bc227a9756308
Rafal Pysz <kuchasz1@o2.pl>
2022-03-21 00:28:24 +0100

    build.tweak.6

--- mobile/src/components/time-keeper-icon.tsx
+++ mobile/src/components/time-keeper-icon.tsx
@@ -1,6 +1,6 @@
 import { Icon } from "@mdi/react";
 import { mdiFlagCheckered, mdiRayStartArrow, mdiRayVertex } from "@mdi/js";
-import { TimeKeeperType } from "../../../timer/model";
+import { TimeKeeperType } from "@set/timer/model";
 
 const getIconFromType = (type: TimeKeeperType) => {
     switch (type) {
