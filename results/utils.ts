4d404678e9e00af48e1b785bff5475cd861b8bc1
Rafal Pysz <kuchasz1@o2.pl>
2022-03-27 13:59:45 +0200

    timer beep another try .10

--- results/utils.ts
+++ results/utils.ts
@@ -28,10 +28,10 @@ export const createBeep = () => {
     };
 
     return ((freq = 520, duration = 500, vol = 100) => {
-        if (context.state !== "running") {
-            context.resume().then(() => runBeep(freq, duration, vol));
-        } else {
-            runBeep(freq, duration, vol);
-        }
+        // if (context.state !== "running") {
+        //     context.resume().then(() => runBeep(freq, duration, vol));
+        // } else {
+        runBeep(freq, duration, vol);
+        // }
     }) as BeepFunction;
 };
