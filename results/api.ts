c3f2ff5512ed3821f37fdc8f11606b9daaf76554
Rafal Pysz <kuchasz1@o2.pl>
2022-04-06 01:07:58 +0200

    NTP time sync tweaks

--- results/api.ts
+++ results/api.ts
@@ -8,31 +8,3 @@ export const getPlayers = (): Promise<Player[]> => fetch(`${hubUrl}/players`).th
 export const getPlayersDate = (): Promise<number> => fetch(`${hubUrl}/players-date`).then((x) => x.json());
 export const getTimerPlayers = (): Promise<ClockListPlayer[]> => fetch(`${hubUrl}/clock-players`).then((x) => x.json());
 export const timeSyncUrl = `${hubUrl}/timesync`;
-// export const getCurrentTimeOffset = (): Promise<number> => {
-//     return new Promise<number>(async (res) => {
-//         const clientTime = Date.now();
-//         const { serverTime } = await fetch(`${hubUrl}/current-time`).then((x) => x.json());
-//         var returned = Date.now();
-//         var timestamp = [clientTime, serverTime;
-//         var original = +timestamp[0];
-//         var receive = +timestamp[1];
-//         var transmit = +timestamp[2];
-//         var sending = receive - original;
-//         var receiving = returned - transmit;
-//         var roundtrip = sending + receiving;
-//         var oneway = roundtrip / 2;
-//         var difference = sending - oneway;
-
-//         return difference;
-//         // const clientTime = Date.now();
-//         // const reqSendTime = Date.now();
-//         // const { serverTime } = await fetch(`${hubUrl}/current-time`).then((x) => x.json());
-//         // const reqBackTime = Date.now();
-
-//         // const delay = (reqBackTime - reqSendTime) / 2;
-
-//         // console.log("delay: ", delay);
-
-//         // res(clientTime - (serverTime + delay));
-//     });
-// };
