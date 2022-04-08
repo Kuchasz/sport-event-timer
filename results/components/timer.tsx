e8832b98dccf011be59b36035a00c17b974aa881
Rafal Pysz <kuchasz1@o2.pl>
2022-04-08 22:35:59 +0200

    build.fix.1

--- results/components/timer.tsx
+++ results/components/timer.tsx
@@ -2,7 +2,7 @@ import { formatTimeNoSec } from "@set/shared/dist";
 
 const Time = ({ time, fontSize }: { time: number; fontSize: number }) => (
     <div style={{ fontSize: `${fontSize}rem` }} className="leading-none text-white text-center transition-all">
-        {formatTimeNoSec(new Date(time))}
+        {formatTimeNoSec(time)}
     </div>
 );
 
