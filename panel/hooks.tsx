4a7911c763ded6b568be47ba1d3d5d198bddc971
Rafal Pysz <kuchasz1@o2.pl>
2022-07-19 00:31:05 +0200

    fixed other hook usages

--- panel/hooks.tsx
+++ panel/hooks.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const useFormState = <T,>(initialFormState: T) => {
+export const useFormState = <T,>(initialFormState: T, depts: unknown[]) => {
     const [state, setState] = useState({ ...initialFormState });
 
     const fieldChangeHandler =
@@ -9,7 +9,7 @@ export const useFormState = <T,>(initialFormState: T) => {
             setState(prev => ({ ...prev, [prop]: e?.target?.value }));
 
     const reset = () => setState({ ...initialFormState });
-    useEffect(reset, [initialFormState]);
+    useEffect(reset, depts);
 
     return [state, fieldChangeHandler, reset] as const;
 };
