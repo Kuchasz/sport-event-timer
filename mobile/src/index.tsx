58c7be810282e83a8dbcd566a5a1710a6dc56181
Rafal Pysz <kuchasz1@o2.pl>
2022-07-30 06:33:26 +0200

    first ci cd tries

--- mobile/src/index.tsx
+++ mobile/src/index.tsx
@@ -81,11 +81,11 @@ const store = createStore([persistStateMiddleware, addIssuerMiddleware, postActi
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-    <React.StrictMode>
-        <ReduxStoreProvider store={store}>
-            <LoggedApp />
-        </ReduxStoreProvider>
-    </React.StrictMode>
+    // <React.StrictMode>
+    <ReduxStoreProvider store={store}>
+        <LoggedApp />
+    </ReduxStoreProvider>
+    // </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
