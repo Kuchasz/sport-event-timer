6db3ef5b044e809912cfdfc49469a5ae9d8a723d
Kuchasz <kuchasz1@o2.pl>
2022-08-15 21:23:41 +0200

    race specific stopwatch seems to work

--- panel/pages/stopwatch/[raceId]/pad.tsx
+++ panel/pages/stopwatch/[raceId]/pad.tsx
@@ -2,14 +2,13 @@ import { add } from "@set/timer/dist/slices/time-stamps";
 import { getCurrentTime } from "@set/shared/dist";
 import { useTimerDispatch } from "../../../hooks";
 import { PlayersCheckIn } from "components/stopwatch/players-check-in";
+import { timeKeeperIdAtom, timeOffsetAtom } from "stopwatch-states";
+import { useAtom } from "jotai";
 
-type PlayersDialPadProps = {
-    timeKeeperId: number;
-    offset: number;
-};
-
-const PlayersDialPad = ({ timeKeeperId, offset }: PlayersDialPadProps) => {
+const PlayersDialPad = () => {
     const dispatch = useTimerDispatch();
+    const [timeKeeperId] = useAtom(timeKeeperIdAtom);
+    const [offset] = useAtom(timeOffsetAtom);
 
     return (
         <PlayersCheckIn
