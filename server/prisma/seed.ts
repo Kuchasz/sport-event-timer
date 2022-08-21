3035f1778037b116ccd39852480f3c82006d9c70
Kuchasz <kuchasz1@o2.pl>
2022-08-21 23:02:08 +0200

    seed improvements

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,7 +1,7 @@
-import { faker } from "@faker-js/faker";
+import { faker } from "@faker-js/faker/locale/pl";
 import { db } from "../db";
 import { createRange } from "@set/shared/dist/index";
-import { Classification, Player } from "@prisma/client";
+import { Classification, Player, Race, TimingPoint } from "@prisma/client";
 
 async function main() {
     const adminUser = await db.user.findFirst({ where: { id: 1 } });
@@ -17,21 +17,23 @@ async function main() {
         }
     });
 
-    const _races = createRange({ from: 0, to: faker.mersenne.rand(20, 10) }).map(() => ({
-        date: faker.date.future(1),
-        name: faker.company.name()
-    }));
-
+    const _races = createRaces();
     const races = await db.$transaction(_races.map(data => db.race.create({ data })));
 
     const _classifications = createClassifications(races.map(r => r.id));
-
     const classifications = await db.$transaction(_classifications.map(data => db.classification.create({ data })));
 
     const _players = createPlayers(Object.fromEntries(races.map(r => [r.id, {}])), ['male', 'female'], user.id, classifications);
-
     const players = await db.$transaction(_players.map(data => db.player.create({ data })));
+
+    const _timingPoints = createTimingPoints(races.map(r => r.id));
+    const timingPoints = await db.$transaction(_timingPoints.map(data => db.timingPoint.create({ data })))
 }
+
+const createRaces = (): Omit<Race, "id">[] => createRange({ from: 0, to: faker.mersenne.rand(20, 10) }).map(() => ({
+    date: faker.date.future(1),
+    name: faker.company.name()
+}));
 
 const createClassifications = (raceIds: number[]): Omit<Classification, "id">[] =>
     raceIds.flatMap(r =>
@@ -63,6 +65,13 @@ const createPlayers = (stores: { [key: number]: {} }, genders: string[], userId:
             }))
     }
     );
+
+const createTimingPoints = (raceIds: number[]): Omit<TimingPoint, "id">[] =>
+    raceIds.flatMap(r => [
+        { name: 'Start', order: 1, raceId: r },
+        ...createRange({ from: 0, to: faker.mersenne.rand(2, 0) })
+            .map((i) => ({ name: faker.word.noun(), order: 1 + i, raceId: r })),
+        { name: 'Finish', order: 5, raceId: r }]);
 
 main()
     .then(async () => {
