ba7095b66f4a0d30e393f3b66126087a81b27b6d
Kuchasz <kuchasz1@o2.pl>
2022-08-22 01:26:52 +0200

    server dependencies cleanup

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,9 @@
-import * as trpcExpress from "@trpc/server/adapters/express";
-import * as trpcWs from "@trpc/server/adapters/ws";
-import cors from "cors";
-import express from "express";
 import { appRouter } from "./router";
 import { createContext } from "./trpc-context";
-import { createServer } from "http";
-import * as trpc from '@trpc/server';
-import { createHTTPHandler, createHTTPServer } from '@trpc/server/adapters/standalone';
+import { createHTTPHandler } from '@trpc/server/adapters/standalone';
 import { applyWSSHandler } from '@trpc/server/adapters/ws';
 import ws from 'ws';
-import { z } from 'zod';
 import * as http from "http";
-
 
 export interface TypedRequestBody<T> extends Express.Request {
     body: T;
@@ -47,8 +39,6 @@ applyWSSHandler<AppRouter>({
 
 server.listen(3001);
 
-// const app = express();
-// const server = createServer(app);
 // const corsOptions = {
 //     credentials: true,
 //     preflightContinue: false,
@@ -56,24 +46,3 @@ server.listen(3001);
 //     methods: ["GET", "POST", "PUT", "OPTIONS", "PATCH"],
 //     allowedHeaders: ["Content-Type", "Authorization", "Accept"]
 // };
-
-// const wss = new ws.Server({ server });
-// const handler = trpcWs.applyWSSHandler({ wss, router: appRouter, createContext });
-
-// const trpcExpressMiddleware = trpcExpress.createExpressMiddleware({
-//     router: appRouter,
-//     createContext
-// });
-
-// app.use(cors(corsOptions));
-// app.use("/", trpcExpressMiddleware);
-
-// process.on("SIGTERM", () => {
-//     handler.broadcastReconnectNotification();
-//     wss.close();
-//     server.close();
-// });
-
-// server.listen(3001, "localhost", () => {
-//     console.log("SERVER_STARTED_LISTENING");
-// });
