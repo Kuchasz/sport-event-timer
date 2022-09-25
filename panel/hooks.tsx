273dcc53fd48f26630273c9ee83c647c6c0c1c79
Kuchasz <kuchasz1@o2.pl>
2022-09-25 16:47:09 +0200

    times list time animation

--- panel/hooks.tsx
+++ panel/hooks.tsx
@@ -1,6 +1,6 @@
 import { CurrentRaceContext } from "current-race-context";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import type { TimerState, TimerDispatch } from "@set/timer/dist/store";
 
 export const useFormState = <T,>(initialFormState: T, depts: unknown[]) => {
@@ -9,7 +9,7 @@ export const useFormState = <T,>(initialFormState: T, depts: unknown[]) => {
     const fieldChangeHandler =
         <K extends keyof T>(prop: K) =>
         (e: { target: { value: T[K] } }) =>
-            setState(prev => ({ ...prev, [prop]: e?.target?.value }));
+            setState((prev) => ({ ...prev, [prop]: e?.target?.value }));
 
     const reset = () => setState({ ...initialFormState });
     useEffect(reset, depts);
@@ -20,3 +20,13 @@ export const useFormState = <T,>(initialFormState: T, depts: unknown[]) => {
 export const useTimerDispatch = () => useDispatch<TimerDispatch>();
 export const useTimerSelector: TypedUseSelectorHook<TimerState> = useSelector;
 export const useCurrentRaceId = () => useContext(CurrentRaceContext).raceId;
+export const usePreviousValue = <T,>(value: T) => {
+    const ref = useRef<T>();
+
+    useEffect(() => {
+        if (value === undefined || value === null) return;
+        ref.current = value;
+    }, [value]);
+
+    return ref.current;
+};
