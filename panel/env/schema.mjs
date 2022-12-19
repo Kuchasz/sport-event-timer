94977183b38e7a91da710c0e60242a54dd3ffd2c
Kuchasz <kuchasz1@o2.pl>
2022-12-19 19:08:54 +0100

    added nextauth secret handling

--- panel/env/schema.mjs
+++ panel/env/schema.mjs
@@ -15,6 +15,7 @@ export const serverSchema = z.object({
     // VERCEL_URL doesnt include `https` so it cant be validated as a URL
     process.env.VERCEL ? z.string() : z.string().url(),
   ),
+  NEXTAUTH_SECRET: z.string(),
   AUTH0_CLIENT_ID: z.string(),
   AUTH0_CLIENT_SECRET: z.string(),
   AUTH0_ISSUER: z.string()
