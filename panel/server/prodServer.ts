fdae0b72fd35317e409520a1e2b47a432e5488f1
Kuchasz <kuchasz1@o2.pl>
2022-12-23 14:47:31 +0100

    trying to use dotenv

--- panel/server/prodServer.ts
+++ panel/server/prodServer.ts
@@ -6,9 +6,10 @@ import { parse } from 'url';
 import ws from 'ws';
 import { appRouter } from './routers/app';
 import { logger } from '../utils';
+import { env } from 'env/server.mjs';
 
-const port = parseInt(process.env.PORT || '3000', 10);
-const dev = process.env.NODE_ENV !== 'production';
+const port = parseInt(env.PORT || '3000', 10);
+const dev = env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
