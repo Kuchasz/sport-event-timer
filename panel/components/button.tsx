074aa8073e1bdb9c204387d65dd66858e75f65b6
Kuchasz <kuchasz1@o2.pl>
2023-01-08 15:42:25 +0100

    small ux chages

--- panel/components/button.tsx
+++ panel/components/button.tsx
@@ -7,7 +7,7 @@ export const Button = ({ outline, children, className, ...props }: ButtonProps) 
         <button
             {...props}
             type="button"
-            className={`flex items-center justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 ${className}`}
+            className={`flex items-center justify-center rounded-md border border-transparent bg-blue-100 px-4 py-1.5 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 ${className}`}
         >
             {children}
         </button>
