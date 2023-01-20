b7c50cfc0ab5767be2853efc961359f98c5b6007
Kuchasz <kuchasz1@o2.pl>
2023-01-20 21:56:41 +0100

    build fix 1

---
+++ panel/server/prisma/migrations/20230120044147_another_small_cleanup/migration.sql
@@ -0,0 +1,8 @@
+DELETE FROM 'Session'
+WHERE userId = 'cld1hrpju0000rpqyvf6ylivo';
+
+DELETE FROM 'Account'
+WHERE userId = 'cld1hrpju0000rpqyvf6ylivo';
+
+DELETE FROM 'User'
+WHERE id = 'cld1hrpju0000rpqyvf6ylivo';
