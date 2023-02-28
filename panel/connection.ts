86ede5030c942e28e3693ef46aee5978f6637589
Kuchasz <kuchasz1@o2.pl>
2023-03-01 00:02:43 +0100

    connection state handler to timer added

--- panel/connection.ts
+++ panel/connection.ts
@@ -87,4 +87,4 @@ export const onConnectionStateChanged = (handler: ConnectionStateHandler) => {
     };
 }; //possible memory leaks
 
-export const allowedLatency = 200;
+export const allowedLatency = 50;
