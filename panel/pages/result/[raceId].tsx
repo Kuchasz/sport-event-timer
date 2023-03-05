c0c50080331efa1e755ef8281f9cf93b73be6e9f
Kuchasz <kuchasz1@o2.pl>
2023-03-06 00:36:48 +0100

    results pages tweak

--- panel/pages/result/[raceId].tsx
+++ panel/pages/result/[raceId].tsx
@@ -1,5 +1,6 @@
 import { formatTimeWithMilliSecUTC } from "@set/utils/dist/datetime";
 import { trpc } from "connection";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 const Result = () => {
@@ -9,41 +10,46 @@ const Result = () => {
     const { data: results } = trpc.result.results.useQuery({ raceId: parseInt(raceId! as string) }, { enabled: raceId !== undefined });
 
     return (
-        <div className="container flex justify-center mx-auto">
-            <div className="flex flex-col">
-                <div className="w-full">
-                    <div className="border-b border-gray-200 shadow">
-                        <table className="divide-y divide-gray-300 ">
-                            <thead className="bg-gray-50">
-                                <tr>
-                                    <th className="px-6 py-2 text-xs text-gray-500">#</th>
-                                    <th className="px-6 py-2 text-xs text-gray-500">Bib</th>
-                                    <th className="px-6 py-2 text-xs text-gray-500">Name</th>
-                                    <th className="px-6 py-2 text-xs text-gray-500">Last Name</th>
-                                    <th className="px-6 py-2 text-xs text-gray-500">Team</th>
-                                    <th className="px-6 py-2 text-xs text-gray-500">Gender</th>
-                                    <th className="px-6 py-2 text-xs text-gray-500">Result</th>
-                                </tr>
-                            </thead>
-                            <tbody className="bg-white divide-y divide-gray-300">
-                                {results &&
-                                    results.map((s, i) => (
-                                        <tr className="whitespace-nowrap">
-                                            <td className="px-6 py-4">{i + 1}</td>
-                                            <td className="px-6 py-4">{s.bibNumber}</td>
-                                            <td className="px-6 py-4">{s.name}</td>
-                                            <td className="px-6 py-4">{s.lastName}</td>
-                                            <td className="px-6 py-4">{s.team ?? ""}</td>
-                                            <td className="px-6 py-4">{s.gender}</td>
-                                            <td className="px-6 py-4">{formatTimeWithMilliSecUTC(s.result)}</td>
-                                        </tr>
-                                    ))}
-                            </tbody>
-                        </table>
+        <>
+            <Head>
+                <title>Results</title>
+            </Head>
+            <div className="container flex justify-center mx-auto">
+                <div className="flex flex-col">
+                    <div className="w-full">
+                        <div className="border-b border-gray-200 shadow">
+                            <table className="divide-y divide-gray-300 ">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th className="px-6 py-2 text-xs text-gray-500">#</th>
+                                        <th className="px-6 py-2 text-xs text-gray-500">Bib</th>
+                                        <th className="px-6 py-2 text-xs text-gray-500">Name</th>
+                                        <th className="px-6 py-2 text-xs text-gray-500">Last Name</th>
+                                        <th className="px-6 py-2 text-xs text-gray-500">Team</th>
+                                        <th className="px-6 py-2 text-xs text-gray-500">Gender</th>
+                                        <th className="px-6 py-2 text-xs text-gray-500">Result</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-300">
+                                    {results &&
+                                        results.map((s, i) => (
+                                            <tr className="whitespace-nowrap">
+                                                <td className="px-6 py-4">{i + 1}</td>
+                                                <td className="px-6 py-4">{s.bibNumber}</td>
+                                                <td className="px-6 py-4">{s.name}</td>
+                                                <td className="px-6 py-4">{s.lastName}</td>
+                                                <td className="px-6 py-4 text-ellipsis">{s.team ?? ""}</td>
+                                                <td className="px-6 py-4">{s.gender}</td>
+                                                <td className="px-6 py-4">{formatTimeWithMilliSecUTC(s.result)}</td>
+                                            </tr>
+                                        ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
