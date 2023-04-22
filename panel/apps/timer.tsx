5e31a987e3f562c873339af78987be2e59551ec3
Kuchasz <kuchasz1@o2.pl>
2023-04-23 01:34:03 +0200

    removed sw

--- panel/apps/timer.tsx
+++ panel/apps/timer.tsx
@@ -5,18 +5,18 @@ type TimerAppProps = AppProps;
 
 export function TimerApp({ Component, pageProps }: TimerAppProps) {
     useEffect(() => {
-        if ("serviceWorker" in navigator) {
-            window.addEventListener("load", function () {
-                navigator.serviceWorker.register("/sw.js").then(
-                    function (registration) {
-                        console.log("Service Worker registration successful with scope: ", registration.scope);
-                    },
-                    function (err) {
-                        console.log("Service Worker registration failed: ", err);
-                    }
-                );
-            });
-        }
+        // if ("serviceWorker" in navigator) {
+        //     window.addEventListener("load", function () {
+        //         navigator.serviceWorker.register("/sw.js").then(
+        //             function (registration) {
+        //                 console.log("Service Worker registration successful with scope: ", registration.scope);
+        //             },
+        //             function (err) {
+        //                 console.log("Service Worker registration failed: ", err);
+        //             }
+        //         );
+        //     });
+        // }
     }, []);
 
     return <Component {...pageProps} />;
