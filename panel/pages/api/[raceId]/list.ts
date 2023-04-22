e5351d8304b49a711ffb633dc7abb30c31932b87
Kuchasz <kuchasz1@o2.pl>
2023-04-22 23:50:27 +0200

    updated apis for 3rd party apps

--- panel/pages/api/[raceId]/list.ts
+++ panel/pages/api/[raceId]/list.ts
@@ -7,7 +7,9 @@ const getRegisteredPlayersForRace = async (req: NextApiRequest, res: NextApiResp
 
     const { raceId } = req.query;
 
-    const results = await db.playerRegistration.findMany({ where: { raceId: Number(raceId) } });
+    const results = await db.playerRegistration.findMany({ 
+        where: { raceId: Number(raceId) }, 
+        select: { name: true, lastName: true, team: true, city: true } });
 
     res.json(results);
 }
