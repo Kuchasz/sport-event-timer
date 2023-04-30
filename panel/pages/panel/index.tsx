fe64bc4d55692429d275f069a80cd28fb9434626
Kuchasz <kuchasz1@o2.pl>
2023-04-30 23:23:41 +0200

    very simple dashboard added

--- panel/pages/panel/index.tsx
+++ panel/pages/panel/index.tsx
@@ -1,15 +1,19 @@
-
+import { trpc } from "connection";
 import Head from "next/head";
 
-
 const Index = () => {
+    const { data: dashboardData } = trpc.race.raport.useQuery();
+
     return (
         <>
             <Head>
                 <title>Home</title>
             </Head>
             <div className="border-1 flex flex-col h-full border-gray-600 border-solid">
-                Nothing here
+                <div className="self-start shadow-md p-4 rounded-md flex flex-col items-center">
+                    <div className="font-semibold text-3xl">{dashboardData?.numberOfRaces}</div>
+                    <div>number of races</div>
+                </div>
             </div>
         </>
     );
