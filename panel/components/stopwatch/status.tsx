f55047910c697bbba15a2a3dc223f8b8669bc131
Kuchasz <kuchasz1@o2.pl>
2023-05-07 15:35:12 +0200

    connection state bar changes

--- panel/components/stopwatch/status.tsx
+++ panel/components/stopwatch/status.tsx
@@ -1,6 +1,6 @@
 import { ConnectionState, trpc } from "../../connection";
 import { Icon } from "@mdi/react";
-import { mdiCloudOffOutline, mdiCloudOutline, mdiCloudSyncOutline, mdiWeatherCloudyAlert } from "@mdi/js";
+import { mdiCloudOffOutline, mdiSync, mdiWeatherCloudyAlert } from "@mdi/js";
 import { TimingPointIcon } from "./timing-point-icon";
 import { Timer } from "./timer";
 import { useAtom } from "jotai";
@@ -9,35 +9,6 @@ import { connectionStateAtom, timingPointIdAtom, timeOffsetAtom } from "states/s
 import { useRouter } from "next/router";
 
 import classNames from "classnames";
-
-const getIconFromConnectionState = (state: ConnectionState) => {
-    switch (state) {
-        case "connected":
-            return mdiCloudOutline;
-        case "disconnected":
-            return mdiCloudOffOutline;
-        case "error":
-            return mdiWeatherCloudyAlert;
-        case "connecting":
-            return mdiCloudSyncOutline;
-        default:
-            throw new Error("not handled connection state");
-    }
-};
-
-const getTextFromConnectionState = (state: ConnectionState) => {
-    switch (state) {
-        case "connected":
-            return "ON-LINE";
-        case "disconnected":
-        case "error":
-            return "OFF-LINE";
-        case "connecting":
-            return "CONNECTING";
-        default:
-            throw new Error("not handled connection state");
-    }
-};
 
 const SelectedTimingPoint = ({
     timingPoints,
@@ -61,40 +32,106 @@ const WarningMessage = ({ contents }: { contents: string }) => (
     </span>
 );
 
+const StatusBar = ({
+    displayOn: desiredState,
+    color,
+    icon,
+    text,
+    connectionState,
+    pulse = false,
+    spin = false
+}: {
+    displayOn: ConnectionState;
+    color: React.ComponentProps<"div">["className"];
+    icon: string;
+    text: string;
+    connectionState: ConnectionState;
+    pulse?: boolean;
+    spin?: boolean;
+}) => (
+    <span
+        className={classNames(
+            `text-xs w-full overflow-hidden text-white font-semibold ease-out transition-all flex justify-center items-center ${color}`,
+            {
+                ['h-0']:  connectionState !== desiredState,
+                ["h-auto py-2"]: connectionState === desiredState,
+                ["animate-pulse"]: pulse,
+            }
+        )}
+    >
+        <span
+            className={classNames("transition-opacity flex items-center", {
+                ["opacity-0"]: connectionState !== desiredState,
+                ["opacity-100"]: connectionState === desiredState,
+            })}
+        >
+            <div className="mr-2">{text}</div>
+            <Icon
+                className={classNames({
+                    ["animate-spin"]: spin,
+                })}
+                path={icon}
+                size={0.8}
+            />
+        </span>
+    </span>
+);
+
 export const Status = () => {
     const [connectionState] = useAtom(connectionStateAtom);
     const {
         query: { raceId },
     } = useRouter();
-    const { data: allTimingPoints } = trpc.timingPoint.timingPoints.useQuery({ raceId: parseInt(raceId as string)}, {
-        initialData: [],
-    });
-    const {data: timingPointOrder } = trpc.timingPoint.timingPointsOrder.useQuery({raceId: parseInt(raceId as string)}, {initialData: []});
+    const { data: allTimingPoints } = trpc.timingPoint.timingPoints.useQuery(
+        { raceId: parseInt(raceId as string) },
+        {
+            initialData: [],
+        }
+    );
+    const { data: timingPointOrder } = trpc.timingPoint.timingPointsOrder.useQuery(
+        { raceId: parseInt(raceId as string) },
+        { initialData: [] }
+    );
     const [timingPointId] = useAtom(timingPointIdAtom);
     const [offset] = useAtom(timeOffsetAtom);
-    const timingPointName = allTimingPoints!.find((tk) => tk.id === timingPointId)?.name;
+    const timingPointName = allTimingPoints!.find(tk => tk.id === timingPointId)?.name;
     const sortedTimingPoints = timingPointOrder;
 
     return (
         <div>
-            <span
-                className={classNames("text-xs w-full text-white font-semibold transition-all h-0 flex justify-center items-center", {
-                    ["invisible"]: connectionState === "connected",
-                    ["visible h-auto py-2"]: connectionState !== "connected",
-                    ["bg-red-600"]: connectionState === "disconnected" || connectionState === "error",
-                    ["bg-orange-600"]: connectionState === "connecting",
-                })}
-            >
-                <span className="mr-2">{getTextFromConnectionState(connectionState)}</span>
-                <Icon path={getIconFromConnectionState(connectionState)} size={0.8} />
-            </span>
+            <StatusBar
+                color="bg-orange-600"
+                icon={mdiSync}
+                displayOn="connecting"
+                text="CONNECTING"
+                connectionState={connectionState}
+                spin
+            />
+            <StatusBar
+                color="bg-red-600"
+                icon={mdiWeatherCloudyAlert}
+                displayOn="error"
+                text="CONNECTION ERROR"
+                connectionState={connectionState}
+                pulse
+            />
+            <StatusBar
+                color="bg-red-600"
+                icon={mdiCloudOffOutline}
+                displayOn="disconnected"
+                text="OFF-LINE"
+                connectionState={connectionState}
+            />
             <div className="px-4 py-2 rounded-b-lg z-10 bg-black text-white w-screen justify-between flex-shrink-0 flex items-center font-semibold">
                 <Timer offset={offset!} />
                 <Link href={`/stopwatch/${raceId}/config`}>
                     <span>
                         {sortedTimingPoints.length === 0 ? (
                             <WarningMessage contents={"NO TIMING POINTS"} />
-                        ) : timingPointId === undefined || timingPointId === null || timingPointId === 0 || !sortedTimingPoints.includes(timingPointId) ? (
+                        ) : timingPointId === undefined ||
+                          timingPointId === null ||
+                          timingPointId === 0 ||
+                          !sortedTimingPoints.includes(timingPointId) ? (
                             <WarningMessage contents={"SET TIMING POINT"} />
                         ) : (
                             <SelectedTimingPoint
