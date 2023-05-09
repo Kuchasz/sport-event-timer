bb587f6f8d1f30cdf9df0ea2ffb548bbb6026a99
Kuchasz <kuchasz1@o2.pl>
2023-05-10 00:54:53 +0200

    poor checkbox tweak

--- panel/components/poor-checkbox.tsx
+++ panel/components/poor-checkbox.tsx
@@ -2,14 +2,16 @@ import { Input } from "./input";
 
 export const PoorCheckbox = ({
     value,
-    onChange
+    onChange,
 }: {
     value?: boolean | null;
     onChange: (event: { target: { value: boolean } }) => void;
 }) => (
     <Input
         type="checkbox"
-        checked={value || false}
-        onChange={e => onChange({ target: { value: Boolean(e.currentTarget.value) } })}
+        checked={value!}
+        onChange={e => {
+            onChange({ target: { value: !value } });
+        }}
     />
 );
