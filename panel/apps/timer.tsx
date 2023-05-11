590fb24201d841248688200a8abf2d5d3f16a576
Kuchasz <kuchasz1@o2.pl>
2023-05-12 01:42:18 +0200

    timer manifest added

--- panel/apps/timer.tsx
+++ panel/apps/timer.tsx
@@ -1,23 +1,14 @@
 import { AppProps } from "next/app";
-// import { useEffect } from "react";
+import Head from "next/head";
 
 type TimerAppProps = AppProps;
 
-export function TimerApp({ Component, pageProps }: TimerAppProps) {
-    // useEffect(() => {
-    //     // if ("serviceWorker" in navigator) {
-    //     //     window.addEventListener("load", function () {
-    //     //         navigator.serviceWorker.register("/sw.js").then(
-    //     //             function (registration) {
-    //     //                 console.log("Service Worker registration successful with scope: ", registration.scope);
-    //     //             },
-    //     //             function (err) {
-    //     //                 console.log("Service Worker registration failed: ", err);
-    //     //             }
-    //     //         );
-    //     //     });
-    //     // }
-    // }, []);
-
-    return <Component {...pageProps} />;
-}
+export const TimerApp = ({ Component, pageProps }: TimerAppProps) => (
+    <>
+        <Head>
+            <title>Timer</title>
+            <link key="manifest" rel="manifest" href="/favicon/timer.webmanifest" />
+        </Head>
+        <Component {...pageProps} />
+    </>
+);
