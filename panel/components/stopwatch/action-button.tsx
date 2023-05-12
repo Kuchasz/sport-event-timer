15e41f900f997ed0178ac0c478b0a0d5718900c4
Kuchasz <kuchasz1@o2.pl>
2023-05-12 21:44:40 +0200

    absence handling buttons added

--- panel/components/stopwatch/action-button.tsx
+++ panel/components/stopwatch/action-button.tsx
@@ -1,4 +1,5 @@
 import Icon from "@mdi/react";
+import classNames from "classnames";
 
 export const PrimaryActionButton = ({
     onClick,
@@ -21,8 +22,24 @@ export const PrimaryActionButton = ({
     </button>
 );
 
-export const ActionButton = ({ onClick, icon, contents }: { onClick: () => void; icon?: string; contents?: JSX.Element }) => (
-    <button onClick={onClick} className="active:animate-pushIn mx-0.5 bg-zinc-100 flex items-center rounded-xl px-3 py-2">
+export const ActionButton = ({
+    onClick,
+    icon,
+    contents,
+    alert = false,
+}: {
+    onClick: () => void;
+    icon?: string;
+    contents?: JSX.Element;
+    alert?: boolean;
+}) => (
+    <button
+        onClick={onClick}
+        className={classNames("active:animate-pushIn mx-0.5 flex items-center rounded-xl px-3 py-2", {
+            ["bg-red-600 text-white"]: alert,
+            ["bg-zinc-100"]: !alert
+        })}
+    >
         {icon && <Icon path={icon} size={1} />}
         {contents}
     </button>
