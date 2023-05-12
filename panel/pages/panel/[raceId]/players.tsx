e18bcf0f9bba4b408049c1dd6a57c2a2d65b6616
Kuchasz <kuchasz1@o2.pl>
2023-05-12 11:56:28 +0200

    players grid moved to ag-grid

--- panel/pages/panel/[raceId]/players.tsx
+++ panel/pages/panel/[raceId]/players.tsx
@@ -1,4 +1,3 @@
-import DataGrid, { Column } from "react-data-grid";
 import Head from "next/head";
 import Icon from "@mdi/react";
 import { Button } from "components/button";
@@ -12,60 +11,81 @@ import { NiceModal } from "../../../components/modal";
 import { PlayerCreate } from "../../../components/player-create";
 import { PlayerEdit } from "components/player-edit";
 import { useCurrentRaceId } from "../../../hooks";
-
+import { ColDef } from "@ag-grid-community/core";
+import { AgGridReact } from "@ag-grid-community/react";
+import { getGridColumnStateAtom } from "states/grid-states";
+import { useAtom } from "jotai";
+import { useCallback, useRef } from "react";
+import { PoorColumnChooser } from "components/poor-column-chooser";
+import { Gender } from "@set/timer/dist/model";
+import { GenderIcon } from "components/gender-icon";
 
 type Player = AppRouterOutputs["player"]["players"][0];
 type CreatedPlayer = AppRouterInputs["player"]["add"]["player"];
 type EditedPlayer = AppRouterInputs["player"]["add"]["player"];
 
-const columns: Column<Player, unknown>[] = [
-    { key: "index", name: "", width: 10, sortable: false, resizable: false },
+const defaultColumns: ColDef<Player>[] = [
+    { field: "index", width: 25, headerName: "Index", headerClass: "hidden", valueGetter: "node.rowIndex + 1", sortable: false, filter: false },
     {
-        key: "classificationId",
-        name: "Classification Id"
+        field: "classificationId",
+        headerName: "Classification Id",
     },
     {
-        key: "bibNumber",
-        name: "Bib Number"
+        field: "bibNumber",
+        headerName: "Bib Number",
+        sort: "asc",
+        comparator: (valueA, valueB) => valueA - valueB,
+        filter: true,
+        sortable: true,
     },
-    { key: "name", name: "Name" },
-    { key: "lastName", name: "Last Name" },
+    { field: "name", headerName: "Name", sortable: true, filter: true },
+    { field: "lastName", headerName: "Last Name", sortable: true, filter: true },
     {
-        key: "gender",
-        name: "Gender",
-        width: 10
+        field: "gender",
+        headerName: "Gender",
+        sortable: true,
+        filter: true,
+        resizable: true,
+        cellStyle: { justifyContent: "center", display: "flex" },
+        width: 150,
+        cellRenderer: (props: { data: Player }) => <GenderIcon gender={props.data.gender as Gender} />,
     },
     {
-        key: "startTime",
-        name: "Start Time",
-        formatter: props => <div>{milisecondsToTimeString(props.row.startTime)}</div>
+        field: "startTime",
+        headerName: "Start Time",
+        sortable: true,
+        cellRenderer: (props: { data: Player }) => <div>{milisecondsToTimeString(props.data.startTime)}</div>,
     },
-    { key: "birthDate", name: "Birth Date", formatter: props => <div>{props.row.birthDate.toLocaleDateString()}</div> },
-    { key: "country", name: "Country", width: 10 },
-    { key: "city", name: "City" },
-    { key: "team", name: "Team" },
-    { key: "email", name: "Email" },
-    { key: "phoneNumber", name: "Phone Number" },
-    { key: "icePhoneNumber", name: "Ice Phone Number" },
     {
-        key: "actions",
-        width: 15,
-        name: "Actions",
-        formatter: props => <PlayerDeleteButton player={props.row} />
-    }
+        field: "birthDate",
+        headerName: "Birth Date",
+        sortable: true,
+        hide: true,
+        cellRenderer: (props: { data: Player }) => <div>{props.data.birthDate.toLocaleDateString()}</div>,
+    },
+    { field: "country", headerName: "Country", width: 10, sortable: true, filter: true, hide: true },
+    { field: "city", headerName: "City", sortable: true, filter: true, hide: true },
+    { field: "team", headerName: "Team", sortable: true, filter: true, hide: true },
+    { field: "email", headerName: "Email", sortable: true, filter: true, hide: true },
+    { field: "phoneNumber", headerName: "Phone Number", sortable: true, filter: true, hide: true },
+    { field: "icePhoneNumber", headerName: "Ice Phone Number", sortable: true, filter: true, hide: true },
+    {
+        field: "actions",
+        // width: 15,
+        headerName: "Actions",
+        cellRenderer: (props: { context: any; data: Player }) => <PlayerDeleteButton refetch={props.context.refetch} player={props.data} />,
+    },
 ];
 
-const PlayerDeleteButton = ({ player }: { player: Player }) => {
-    const raceId = useCurrentRaceId();
-    const { refetch } = trpc.player.players.useQuery({ raceId: raceId! });
+const PlayerDeleteButton = ({ player, refetch }: { player: Player; refetch: () => void }) => {
     const deletePlayerMutation = trpc.player.delete.useMutation();
     const deletePlayer = async () => {
         const confirmed = await Demodal.open<boolean>(NiceModal, {
             title: `Delete player`,
             component: Confirmation,
             props: {
-                message: `You are trying to delete the Player ${player.name} ${player.lastName}. Do you want to proceed?`
-            }
+                message: `You are trying to delete the Player ${player.name} ${player.lastName}. Do you want to proceed?`,
+            },
         });
 
         if (confirmed) {
@@ -102,14 +122,26 @@ const Players = () => {
     const { data: players, refetch } = trpc.player.players.useQuery({ raceId: raceId! });
     const addPlayerMutation = trpc.player.add.useMutation();
     const editPlayerMutation = trpc.player.edit.useMutation();
-    
+    const gridRef = useRef<AgGridReact<Player>>(null);
+    const [gridColumnState, setGridColumnState] = useAtom(
+        getGridColumnStateAtom(
+            "players",
+            defaultColumns.map(c => ({ hide: c.hide, colId: c.field! }))
+        )
+    );
+
+    const onFirstDataRendered = useCallback(() => {
+        gridRef.current?.columnApi.applyColumnState({ state: gridColumnState });
+        gridRef.current?.api.sizeColumnsToFit();
+    }, [gridColumnState]);
+
     const openCreateDialog = async () => {
         const player = await Demodal.open<CreatedPlayer>(NiceModal, {
             title: "Create new player",
             component: PlayerCreate,
             props: {
-                raceId: raceId!
-            }
+                raceId: raceId!,
+            },
         });
 
         if (player) {
@@ -124,8 +156,8 @@ const Players = () => {
             component: PlayerEdit,
             props: {
                 raceId: raceId!,
-                editedPlayer
-            }
+                editedPlayer,
+            },
         });
 
         if (player) {
@@ -161,7 +193,7 @@ const Players = () => {
                 <title>Lista zawodników</title>
             </Head>
 
-            <div className="border-1 flex flex-col h-full border-gray-600 border-solid">
+            <div className="ag-theme-material border-1 flex flex-col h-full border-gray-600 border-solid">
                 {/* <div className="pb-4">
                     <h1 className="text-xl font-semibold uppercase">Players</h1>
                     <div className="flex-grow max-w-xs border-t-2 mt-2 border-pink-400"></div>
@@ -171,6 +203,24 @@ const Players = () => {
                         <Icon size={1} path={mdiPlus} />
                     </Button>
                     <div className="px-1"></div>
+                    <PoorColumnChooser
+                        items={defaultColumns}
+                        initialValue={gridColumnState.filter(c => !c.hide).map(c => c.colId)}
+                        valueKey="field"
+                        nameKey="headerName"
+                        onChange={e => {
+                            const visibleColumns = e.target.value as string[];
+                            const notSelectedColumns = gridColumnState.map(c => c.colId).filter(c => !e.target.value.includes(c));
+
+                            gridRef.current?.columnApi.setColumnsVisible(notSelectedColumns as string[], false);
+                            gridRef.current?.columnApi.setColumnsVisible(visibleColumns, true);
+                            gridRef.current?.api.sizeColumnsToFit();
+
+                            const saveState = gridRef.current?.columnApi.getColumnState()!.map(({ colId, hide }) => ({ colId, hide }))!;
+
+                            setGridColumnState(saveState);
+                        }}
+                    />
                     {/* <div className="px-1"></div>
                     <Button autoCapitalize="false">
                         <Icon size={1} path={mdiNumeric} />
@@ -188,15 +238,26 @@ const Players = () => {
                 </div>
                 {/* {gridElement} */}
                 {players && (
-                    <DataGrid className='rdg-light h-full'
-                        defaultColumnOptions={{
-                            sortable: false,
-                            resizable: true
-                        }}
-                        onRowDoubleClick={e => openEditDialog(e)}
-                        columns={columns}
-                        rows={players}
-                    />
+                    // <DataGrid className='rdg-light h-full'
+                    //     defaultColumnOptions={{
+                    //         sortable: false,
+                    //         resizable: true
+                    //     }}
+                    //     onRowDoubleClick={e => openEditDialog(e)}
+                    //     columns={defaultColumns}
+                    //     rows={players}
+                    // />
+                    <AgGridReact<Player>
+                        ref={gridRef}
+                        context={{ refetch }}
+                        onRowDoubleClicked={e => openEditDialog(e.data)}
+                        suppressCellFocus={true}
+                        suppressAnimationFrame={true}
+                        columnDefs={defaultColumns}
+                        rowData={players}
+                        onFirstDataRendered={onFirstDataRendered}
+                        onGridSizeChanged={onFirstDataRendered}
+                    ></AgGridReact>
                 )}
             </div>
         </>
