3c5ab814562804bcb33651a3a5e385554973fee7
Kuchasz <kuchasz1@o2.pl>
2023-05-20 00:58:01 +0200

    small ui tweak

--- panel/pages/panel/index.tsx
+++ panel/pages/panel/index.tsx
@@ -21,19 +21,19 @@ const Index = () => {
                 <DashboardCard title="Number of races">
                     <div className="flex flex-col items-center">
                         <div className="text-5xl my-2 font-semibold">{dashboardData?.totalRaces}</div>
-                        <div className="text-gray-400 font-normal text-base">Future races: {dashboardData?.futureRaces}</div>
-                        <div className="text-gray-400 font-normal text-base">Past races: {dashboardData?.pastRaces}</div>
+                        <div className="text-gray-400 font-normal text-sm">Future races: {dashboardData?.futureRaces}</div>
+                        <div className="text-gray-400 font-normal text-sm">Past races: {dashboardData?.pastRaces}</div>
                     </div>
                 </DashboardCard>
                 {dashboardData?.nextRace && (
                     <DashboardCard title="Incoming race">
                         <div className="flex flex-col items-center">
-                        <div className="self-end font-semibold text-center">{dashboardData.nextRace.date?.toLocaleDateString()}</div>
+                            <div className="self-end font-semibold text-center">{dashboardData.nextRace.date?.toLocaleDateString()}</div>
                             <div className="text-2xl my-2 font-semibold text-center">{dashboardData.nextRace.name}</div>
-                            <div className="text-gray-400 font-normal text-base">
-                                Registrations: {dashboardData?.nextRace.registeredPlayers} / {dashboardData.nextRace.playersLimit}
+                            <div className="text-gray-400 font-normal text-sm">
+                                Registered: {dashboardData?.nextRace.registeredPlayers} / {dashboardData.nextRace.playersLimit}
                             </div>
-                            <div className="text-gray-400 font-normal text-base">
+                            <div className="text-gray-400 font-normal text-sm">
                                 Registration:{" "}
                                 {dashboardData?.nextRace.registrationEnabled ? (
                                     <span className="font-semibold text-green-600">enabled</span>
