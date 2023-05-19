bdc5c9069a6c7ab24ced3274d2a3218cfd62f154
Kuchasz <kuchasz1@o2.pl>
2023-05-20 01:08:01 +0200

    admin races and accounts switched to ag

--- panel/pages/panel/admin/races.tsx
+++ panel/pages/panel/admin/races.tsx
@@ -1,41 +1,45 @@
-import DataGrid, { Column } from "react-data-grid";
 import Head from "next/head";
 import Icon from "@mdi/react";
 import { Button } from "components/button";
 import { Demodal } from "demodal";
 import { AppRouterInputs, AppRouterOutputs } from "trpc";
 import { trpc } from "../../../connection";
-import { mdiPlus, mdiTrashCan } from "@mdi/js";
+import { mdiPlus, mdiRestore, mdiTrashCan } from "@mdi/js";
 import { NiceModal } from "components/modal";
 import { RaceCreate } from "components/race-create";
 import { RaceEdit } from "components/race-edit";
 import { Confirmation } from "components/confirmation";
+import { useCallback, useRef } from "react";
+import { AgGridReact } from "@ag-grid-community/react";
+import { ColDef } from "@ag-grid-community/core";
 
 type Race = AppRouterOutputs["race"]["races"][0];
 type CreatedRace = AppRouterInputs["race"]["add"];
 type EditedRace = AppRouterInputs["race"]["update"];
 
-const columns: Column<Race, unknown>[] = [
-    { key: "id", name: "Id", width: 10 },
-    { key: "name", name: "Name" },
+const defaultColumns: ColDef<Race>[] = [
+    { field: "id", headerName: "Id", sortable: true },
+    { field: "name", headerName: "Name", sortable: true, filter: true },
     {
-        key: "actions",
-        width: 15,
-        name: "Actions",
-        formatter: props => <RaceDeleteButton race={props.row} />
-    }
+        field: "actions",
+        width: 200,
+        headerName: "Actions",
+        cellRenderer: (props: { data: Race; context: { refetch: () => void } }) => (
+            <RaceDeleteButton refetch={props.context.refetch} race={props.data} />
+        ),
+    },
 ];
 
-const RaceDeleteButton = ({ race }: { race: Race }) => {
-    const { refetch } = trpc.race.races.useQuery();
+const RaceDeleteButton = ({ race, refetch }: { race: Race; refetch: () => void }) => {
     const deleteRaceMutation = trpc.race.delete.useMutation();
-    const deletePlayer = async () => {
+    const wipeRaceMutation = trpc.action.wipe.useMutation();
+    const deleteRace = async () => {
         const confirmed = await Demodal.open<boolean>(NiceModal, {
-            title: `Delete player`,
+            title: `Delete race`,
             component: Confirmation,
             props: {
-                message: `You are trying to delete the Race ${race.name} (${race.date}). Do you want to proceed?`
-            }
+                message: `You are trying to delete the Race ${race.name} (${race.date.toLocaleDateString()}). Do you want to proceed?`,
+            },
         });
 
         if (confirmed) {
@@ -43,39 +47,64 @@ const RaceDeleteButton = ({ race }: { race: Race }) => {
             refetch();
         }
     };
+    const wipeRace = async () => {
+        const confirmed = await Demodal.open<boolean>(NiceModal, {
+            title: `Wipe race`,
+            component: Confirmation,
+            props: {
+                message: `You are trying to wipe the Race ${race.name} (${race.date.toLocaleDateString()}). Do you want to proceed?`,
+            },
+        });
+
+        if (confirmed) {
+            await wipeRaceMutation.mutateAsync({ raceId: race.id });
+            refetch();
+        }
+    };
     return (
-        <span className="flex items-center hover:text-red-600 cursor-pointer" onClick={deletePlayer}>
-            <Icon size={1} path={mdiTrashCan} />
-            delete
-        </span>
+        <div className="flex">
+            <span className="flex items-center hover:text-red-600 cursor-pointer" onClick={deleteRace}>
+                <Icon size={1} path={mdiTrashCan} />
+                delete
+            </span>
+            <span className="flex ml-4 items-center hover:text-red-600 cursor-pointer" onClick={wipeRace}>
+                <Icon size={1} path={mdiRestore} />
+                wipe
+            </span>
+        </div>
     );
 };
 
 const Races = () => {
-    const { data: races, refetch } = trpc.race.races.useQuery();
+    const { data: races, refetch } = trpc.race.races.useQuery(undefined, { initialData: [] });
     const updateRaceMutation = trpc.race.update.useMutation();
-    const addRaceMuttaion = trpc.race.add.useMutation();
+    const addRaceMutation = trpc.race.add.useMutation();
+    const gridRef = useRef<AgGridReact<Race>>(null);
 
     const openCreateDialog = async () => {
         const race = await Demodal.open<CreatedRace>(NiceModal, {
             title: "Create new race",
             component: RaceCreate,
-            props: {}
+            props: {},
         });
 
         if (race) {
-            await addRaceMuttaion.mutateAsync(race);
+            await addRaceMutation.mutateAsync(race);
             refetch();
         }
     };
+
+    const onFirstDataRendered = useCallback(() => {
+        gridRef.current?.api.sizeColumnsToFit();
+    }, []);
 
     const openEditDialog = async (editedRace?: Race) => {
         const race = await Demodal.open<EditedRace>(NiceModal, {
             title: "Edit race",
             component: RaceEdit,
             props: {
-                editedRace
-            }
+                editedRace,
+            },
         });
 
         if (race) {
@@ -89,23 +118,24 @@ const Races = () => {
             <Head>
                 <title>Lista wyścigów</title>
             </Head>
-            <div className="border-1 flex flex-col h-full border-gray-600 border-solid">
+            <div className="ag-theme-material border-1 flex flex-col h-full border-gray-600 border-solid">
                 <div className="mb-4 inline-flex">
                     <Button onClick={openCreateDialog}>
                         <Icon size={1} path={mdiPlus} />
                     </Button>
                 </div>
-                {races && (
-                    <DataGrid className='rdg-light h-full'
-                        defaultColumnOptions={{
-                            sortable: false,
-                            resizable: true
-                        }}
-                        onRowDoubleClick={e => openEditDialog(e)}
-                        columns={columns}
-                        rows={races}
-                    />
-                )}
+
+                <AgGridReact<Race>
+                    ref={gridRef}
+                    context={{ refetch }}
+                    onRowDoubleClicked={e => openEditDialog(e.data)}
+                    suppressCellFocus={true}
+                    suppressAnimationFrame={true}
+                    columnDefs={defaultColumns}
+                    rowData={races}
+                    onFirstDataRendered={onFirstDataRendered}
+                    onGridSizeChanged={onFirstDataRendered}
+                ></AgGridReact>
             </div>
         </>
     );
