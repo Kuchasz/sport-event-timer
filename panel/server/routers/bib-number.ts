8d585de64703615fe06cd712eef25887ccad1e67
Kuchasz <kuchasz1@o2.pl>
2023-05-25 18:29:12 +0200

    not letting bib number to be less than 1

--- panel/server/routers/bib-number.ts
+++ panel/server/routers/bib-number.ts
@@ -5,15 +5,20 @@ import { protectedProcedure, router } from "../trpc";
 const bibNumberSchema = z.object({
     id: z.number().nullish(),
     raceId: z.number({ required_error: "raceId is required" }).min(1),
-    number: z.string({ required_error: "number is required" })
+    number: z.string({ required_error: "number is required" }).min(1)
 });
 
 const addRangeBibNumberSchema = z.object({
     raceId: z.number({ required_error: "raceId is required" }).min(1),
-    startNumber: z.number({ required_error: "startNumber is required" }),
+    startNumber: z.number({ required_error: "startNumber is required" }).min(1),
     endNumber: z.number({ required_error: "endNumber is required" }),
     omitDuplicates: z.boolean().nullish()
-});
+}).refine(
+    data => data.endNumber <= data.startNumber, 
+    { 
+        message: "endNumber must be higher than startNumber", 
+        path: ['endNumber'] 
+    });
 
 export const bibNumberRouter =
     router({
