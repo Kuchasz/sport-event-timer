9f4c40ad6a802fd65e59c67492d0400ab79f0944
Kuchasz <kuchasz1@o2.pl>
2023-07-03 14:31:37 +0200

    email template edit tweaked

---
+++ panel/components/poor-text-area.tsx
@@ -0,0 +1,11 @@
+import { TextArea } from "./text-area";
+
+export const PoorTextArea = ({
+    placeholder,
+    value,
+    onChange
+}: {
+    placeholder?: string;
+    value?: string | null;
+    onChange: (event: { target: { value: string } }) => void;
+}) => <TextArea value={value || ""} placeholder={placeholder   } onChange={e => onChange({ target: { value: e.currentTarget.value } })} />;
