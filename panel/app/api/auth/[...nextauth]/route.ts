19a552b8194329a9cd99dd62219c8a18da85ed06
Kuchasz <kuchasz1@o2.pl>
2023-07-11 23:28:50 +0200

    fixed build issues v5

--- panel/app/api/auth/[...nextauth]/route.ts
+++ panel/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth/next"
 import { authOptions } from "server/auth"
 
-const handler = NextAuth({
+const handler = (req: Request, res: Response) => req ? NextAuth({
     ...authOptions()
-})
+})(req, res) : null;
 
 export { handler as GET, handler as POST }
