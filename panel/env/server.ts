5aa4f0aae6ee5e424ad13c0202adace2053fff0f
Kuchasz <kuchasz1@o2.pl>
2023-07-17 19:42:10 +0200

    fixed build issues v7

--- panel/env/server.ts
+++ panel/env/server.ts
@@ -10,12 +10,14 @@ import { env as clientEnv, formatErrors } from "./client";
 const _serverEnv = serverSchema.safeParse(process.env);
 
 if (!_serverEnv.success) {
-  console.error(
-    "❌ Invalid environment variables:\n",
-    ...formatErrors(_serverEnv.error.format()),
-  );
-  if (process.env.NEXT_PHASE !== PHASE_PRODUCTION_BUILD)
+
+  if (process.env.NEXT_PHASE !== PHASE_PRODUCTION_BUILD) {
+    console.error(
+      "❌ Invalid environment variables:\n",
+      ...formatErrors(_serverEnv.error.format()),
+    );
     throw new Error("Invalid environment variables");
+  }
 }
 
 for (let key of Object.keys(_serverEnv.data)) {
