7a89acbf1ee35d80a3c3ef10cacb5768fedba010
Kuchasz <kuchasz1@o2.pl>
2023-08-24 23:57:43 +0200

    added headers to another pages

--- panel/app/(panel)/panel/admin/dashboard.tsx
+++ panel/app/(panel)/panel/admin/dashboard.tsx
@@ -1,7 +1,9 @@
+import { PageHeader } from "components/page-header";
+
 export const Dashboard = () => {
     return (
         <>
-            <h3>Some dashboard for [ADMIN]</h3>
+            <PageHeader title="Admin Dashboard" description="Monitor what is the most important" />
             <div className="flex w-full relative justify-center overflow-hidden">
                 <div className="w-full my-12 max-w-6xl flex flex-col items-start justify-center"></div>
             </div>
