e11d9b9b98f06c7fe98765d7beb73fe15a553bee
Kuchasz <kuchasz1@o2.pl>
2023-08-29 22:28:20 +0200

    more translations added

--- panel/i18n/en.ts
+++ panel/i18n/en.ts
@@ -21,5 +21,78 @@ export const en = {
             hello: "Say hay!",
             accounts: "Accounts"
         }
+    },
+    pages: {
+        general: {
+            dashboard: {
+                header: {
+                    title: "Dashboard",
+                    description: "Monitor what is the most important"
+                },
+                statistics: {
+                    title: "Statistics",
+                    widgets: {
+                        futureRaces: "Future races",
+                        pastRaces: "Past races"
+                    }
+                },
+                raceStatistics: {
+                    title: "Incoming race statistics",
+                    widgets: {
+                        date: "Date",
+                        name: "Name",
+                        registeredPlayers: "Registered Players",
+                        registrationStatus: "Registration Status"
+                    }
+                }
+            }
+        },
+        registration: {
+            turnOffPopup: {
+                title: "Turn off registration",
+                description: "Online registration will be turned off"
+            },
+            turnOnPopup: {
+                title: "Turn on registration",
+                description: "Online registration will be turned on"
+            },
+        },
+        races: {
+            header: {
+                title: "My Races",
+                description: "Manage all past and future races"
+            },
+            wipeStopwatchPopup: {
+                title: "Wipe stopwatch",
+                description: "Wipe all stopwatch data",
+                confirmation: {
+                    title: "Wipe race data",
+                    description: ""
+                }
+            },
+            deleteRacePopup: {
+                title: "Delete",
+                description: "Delete race",
+                confirmation: {
+                    title: "Delete race",
+                    description: ""
+                }
+            },
+            grid: {
+                columns: {
+                    name: "Name",
+                    date: "Date",
+                    registrationEnabled: "Registration",
+                    registeredPlayers: "Registrations",
+                    actions: "Actions"
+                }
+            },
+            createRace: {
+                title: "Create new race",
+            },
+            editRace: {
+                title: "Create new race",
+            }
+        }
     }
 }
