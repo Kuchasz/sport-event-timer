6bc6f467f58c99f8f7e5b0663f5eb36e3bc50bc4
Kuchasz <kuchasz1@o2.pl>
2023-08-30 18:41:21 +0200

    results translations added

--- panel/app/[locale]/(panel)/panel/[raceId]/results/results.tsx
+++ panel/app/[locale]/(panel)/panel/[raceId]/results/results.tsx
@@ -9,49 +9,9 @@ import { AgGridReact } from "@ag-grid-community/react";
 import { useCallback, useRef } from "react";
 import Head from "next/head";
 import { PageHeader } from "components/page-header";
+import { useTranslations } from "next-intl";
 
 type Result = AppRouterOutputs["result"]["results"][0];
-
-const defaultColumns: ColDef<Result>[] = [
-    { field: "bibNumber", sortable: true, filter: true, headerName: "Bib" },
-    {
-        field: "player.name",
-        sortable: true,
-        filter: true,
-        headerName: "Name",
-        cellRenderer: (p: { data: Result }) => <span>{p.data.name}</span>,
-    },
-    {
-        field: "player.lastName",
-        sortable: true,
-        filter: true,
-        headerName: "Last Name",
-        cellRenderer: (p: { data: Result }) => <span>{p.data.lastName}</span>,
-    },
-    {
-        field: "start",
-        headerName: "Start",
-        sortable: true,
-        cellRenderer: (p: { data: Result }) => <span>{formatTimeWithMilliSec(p.data.start)}</span>,
-    },
-    {
-        field: "finish",
-        headerName: "Finish",
-        sortable: true,
-        cellRenderer: (p: { data: Result }) => <span>{formatTimeWithMilliSec(p.data.finish)}</span>,
-    },
-    {
-        field: "result",
-        headerName: "Result",
-        sort: "asc",
-        sortable: true,
-        cellRenderer: (p: { data: Result }) => (
-            <span className="flex uppercase flex-col items-end font-mono">
-                {p.data.invalidState ? p.data.invalidState : formatTimeWithMilliSecUTC(p.data.result)}
-            </span>
-        ),
-    },
-];
 
 export const Results = () => {
     const raceId = useCurrentRaceId();
@@ -61,13 +21,56 @@ export const Results = () => {
         gridRef.current?.api.sizeColumnsToFit();
     }, []);
 
+    const t = useTranslations();
+
+    const defaultColumns: ColDef<Result>[] = [
+        { field: "bibNumber", sortable: true, filter: true, headerName: t("pages.results.grid.columns.bibNumber") },
+        {
+            field: "player.name",
+            sortable: true,
+            filter: true,
+            headerName: t("pages.results.grid.columns.playerName"),
+            cellRenderer: (p: { data: Result }) => <span>{p.data.name}</span>,
+        },
+        {
+            field: "player.lastName",
+            sortable: true,
+            filter: true,
+            headerName: t("pages.results.grid.columns.playerLastName"),
+            cellRenderer: (p: { data: Result }) => <span>{p.data.lastName}</span>,
+        },
+        {
+            field: "start",
+            headerName: t("pages.results.grid.columns.start"),
+            sortable: true,
+            cellRenderer: (p: { data: Result }) => <span>{formatTimeWithMilliSec(p.data.start)}</span>,
+        },
+        {
+            field: "finish",
+            headerName: t("pages.results.grid.columns.finish"),
+            sortable: true,
+            cellRenderer: (p: { data: Result }) => <span>{formatTimeWithMilliSec(p.data.finish)}</span>,
+        },
+        {
+            field: "result",
+            headerName: t("pages.results.grid.columns.result"),
+            sort: "asc",
+            sortable: true,
+            cellRenderer: (p: { data: Result }) => (
+                <span className="flex uppercase flex-col items-end font-mono">
+                    {p.data.invalidState ? p.data.invalidState : formatTimeWithMilliSecUTC(p.data.result)}
+                </span>
+            ),
+        },
+    ];
+
     return (
         <>
             <Head>
-                <title>Race results</title>
+                <title>{t('pages.results.header.title')}</title>
             </Head>
             <div className="border-1 flex flex-col h-full border-gray-600 border-solid">
-                <PageHeader title="Race Results" description="Results measured by the stopwatch, each may be overriden" />
+                <PageHeader title={t('pages.results.header.title')} description={t('pages.results.header.description')} />
                 {/* <div className="mb-4 inline-flex">
                     <Button onClick={() => {}}>
                         <Icon size={1} path={mdiPlus} />
