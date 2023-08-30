95671fec1aa60087b62f652246cdd984f281b2c6
Kuchasz <kuchasz1@o2.pl>
2023-08-31 01:59:56 +0200

    some more race basic data translations added

--- panel/app/[locale]/(panel)/panel/[raceId]/race.tsx
+++ panel/app/[locale]/(panel)/panel/[raceId]/race.tsx
@@ -3,24 +3,32 @@
 import { DashboardCard } from "components/dashboard-card";
 import { trpc } from "trpc-core";
 import { useCurrentRaceId } from "hooks";
+import { useTranslations } from "next-intl";
+import { PageHeader } from "components/page-header";
 
 export const Race = () => {
     const raceId = useCurrentRaceId();
     const { data: race } = trpc.race.raceRaport.useQuery({ raceId: raceId! }, { enabled: !!raceId });
+    const t = useTranslations();
 
     return (
         <>
             {race && (
                 <div>
                     <div>
-                        <h1 className="text-3xl">{race.name}</h1>
-                        <h2>{race.date.toLocaleDateString()}</h2>
-
+                        <PageHeader title={race.name} description={race.date.toLocaleDateString()} />
                         <div className="mt-8 mb-4">
-                            <div className="mx-3 text-xl font-semibold">Statistics</div>
+                            <div className="mx-3 text-xl font-semibold">{t("pages.race.statistics.header.title")}</div>
                             <div className="flex">
-                                <DashboardCard.Range min={race.registeredPlayers} max={race.playersLimit} title="Registered players" />
-                                <DashboardCard.Discrete enabled={race.registrationEnabled} title="Registration status" />
+                                <DashboardCard.Range
+                                    min={race.registeredPlayers}
+                                    max={race.playersLimit}
+                                    title={t("pages.race.statistics.widgets.registeredPlayers")}
+                                />
+                                <DashboardCard.Discrete
+                                    enabled={race.registrationEnabled}
+                                    title={t("pages.race.statistics.widgets.registrationStatus")}
+                                />
                             </div>
                         </div>
                     </div>
