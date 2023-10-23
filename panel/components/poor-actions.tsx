6e707b15ce6886772718f57f18900ad2eb580e94
Kuchasz <kuchasz1@o2.pl>
2023-10-23 20:02:21 +0200

    poor actions fixed

--- panel/components/poor-actions.tsx
+++ panel/components/poor-actions.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Popover } from "@headlessui/react";
-import type { ReactNode } from "react";
 import { Float } from "@headlessui-float/react";
 import Icon from "@mdi/react";
 import { mdiDotsVertical } from "@mdi/js";
@@ -13,8 +12,6 @@ type Action<TItem> = {
     execute: (param: TItem) => void;
 };
 
-const DIV = ({ children }: { children: ReactNode }) => <div>{children}</div>;
-
 export const PoorActions = <TItem,>({ item, actions }: { item: TItem; actions: Action<TItem>[] }) => {
     return (
         <Popover className="flex h-full items-center">
@@ -22,7 +19,6 @@ export const PoorActions = <TItem,>({ item, actions }: { item: TItem; actions: A
                 transform={false}
                 portal
                 placement="bottom-end"
-                floatingAs={DIV}
                 enter="transition ease-out duration-200"
                 enterFrom="opacity-0 translate-y-1"
                 enterTo="opacity-100 translate-y-0"
