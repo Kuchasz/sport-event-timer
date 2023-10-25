37970071249514a7e7c32c0f33b6b3c6f17f45b6
Kuchasz <kuchasz1@o2.pl>
2023-10-26 01:19:56 +0200

    breadcrumbs improvements

--- panel/app/[locale]/(panel)/[raceId]/@breadcrumbs/bib-numbers/page.tsx
+++ panel/app/[locale]/(panel)/[raceId]/@breadcrumbs/bib-numbers/page.tsx
@@ -1,20 +1,12 @@
 import { Breadcrumbs } from "components/breadcrumbs";
 import { useTranslations } from "next-intl";
-import { db } from "server/db";
 
-const BibNumbersBreadcrumbs = ({ raceId, name }: { raceId: string; name: string }) => {
+export default ({ params }: { params: { raceId: string } }) => {
     const t = useTranslations();
 
     return (
-        <Breadcrumbs>
-            <Breadcrumbs.Item href={`/${raceId}`} text={name}></Breadcrumbs.Item>
+        <Breadcrumbs homePath={`/${params.raceId}`}>
             <Breadcrumbs.Item text={t("pages.bibNumbers.header.title")}></Breadcrumbs.Item>
         </Breadcrumbs>
     );
-};
-
-export default async ({ params }: { params: { raceId: string } }) => {
-    const race = await db.race.findFirstOrThrow({ where: { id: Number(params.raceId) } });
-
-    return <BibNumbersBreadcrumbs raceId={params.raceId} name={race.name} />;
 };
--- panel/app/[locale]/(panel)/[raceId]/@breadcrumbs/player-registrations/page.tsx
+++ panel/app/[locale]/(panel)/[raceId]/@breadcrumbs/player-registrations/page.tsx
@@ -1,20 +1,12 @@
 import { Breadcrumbs } from "components/breadcrumbs";
 import { useTranslations } from "next-intl";
-import { db } from "server/db";
 
-const PlayerRegistrationsBreadcrumbs = ({ raceId, name }: { raceId: string; name: string }) => {
+export default ({ params }: { params: { raceId: string } }) => {
     const t = useTranslations();
 
     return (
-        <Breadcrumbs>
-            <Breadcrumbs.Item href={`/${raceId}`} text={name}></Breadcrumbs.Item>
+        <Breadcrumbs homePath={`/${params.raceId}`}>
             <Breadcrumbs.Item text={t("pages.playerRegistrations.header.title")}></Breadcrumbs.Item>
         </Breadcrumbs>
     );
-};
-
-export default async ({ params }: { params: { raceId: string } }) => {
-    const race = await db.race.findFirstOrThrow({ where: { id: Number(params.raceId) } });
-
-    return <PlayerRegistrationsBreadcrumbs name={race.name} raceId={params.raceId} />;
 };
