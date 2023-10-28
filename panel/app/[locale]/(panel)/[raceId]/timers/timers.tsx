30a145f65c52a0cea166ecf02777c972b74f5428
Kuchasz <kuchasz1@o2.pl>
2023-10-29 00:09:41 +0200

    timers applications initial descriptions added

--- panel/app/[locale]/(panel)/[raceId]/timers/timers.tsx
+++ panel/app/[locale]/(panel)/[raceId]/timers/timers.tsx
@@ -25,34 +25,34 @@ export const Timers = () => {
                                     <Link
                                         href={`/timer/${raceId}`}
                                         target="_blank"
-                                        className="mx-3 flex h-32 w-48 flex-col items-center self-start overflow-clip rounded-md border border-gray-100 p-4 shadow-sm"
+                                        className="mx-3 flex h-32 w-48 shrink-0 flex-col items-center self-start overflow-clip rounded-md border border-gray-100 p-4 shadow-sm"
                                     >
                                         <Icon path={mdiTimer10} size={3} />
                                         <span>Odliczanie (do zera)</span>
                                     </Link>
-                                    <span>Some meaningful description that will highlight some use cases</span>
+                                    <span>{t("pages.timers.applications.countdown.description")}</span>
                                 </div>
                                 <div className="my-3 flex">
                                     <Link
                                         href={`/timer/${raceId}/t`}
                                         target="_blank"
-                                        className="mx-3 flex h-32 w-48 flex-col items-center self-start overflow-clip rounded-md border border-gray-100 p-4 shadow-sm"
+                                        className="mx-3 flex h-32 w-48 shrink-0 flex-col items-center self-start overflow-clip rounded-md border border-gray-100 p-4 shadow-sm"
                                     >
                                         <Icon path={mdiPlaylistPlay} size={3} />
                                         <span>Lista startowa</span>
                                     </Link>
-                                    <span>Some meaningful description that will highlight some use cases</span>
+                                    <span>{t("pages.timers.applications.startList.description")}</span>
                                 </div>
                                 <div className="my-3 flex">
                                     <Link
                                         href={`/timer/${raceId}`}
                                         target="_blank"
-                                        className="mx-3 flex h-32 w-48 flex-col items-center self-start overflow-clip rounded-md border border-gray-100 p-4 shadow-sm"
+                                        className="mx-3 flex h-32 w-48 shrink-0 flex-col items-center self-start overflow-clip rounded-md border border-gray-100 p-4 shadow-sm"
                                     >
                                         <Icon path={mdiTimerOutline} size={3} />
-                                        <span>Stoper (do zera)</span>
+                                        <span>Stoper (od zera)</span>
                                     </Link>
-                                    <span>Some meaningful description that will highlight some use cases</span>
+                                    <span>{t("pages.timers.applications.stopwatch.description")}</span>
                                 </div>
                             </div>
                         </div>
