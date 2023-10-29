8dec7f96d73f06a721460ba1a173e86224db30aa
Kuchasz <kuchasz1@o2.pl>
2023-10-29 02:21:41 +0200

    better menu header alignment

--- panel/components/menu-header.tsx
+++ panel/components/menu-header.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
 
 export const MenuHeader = (n: { text: string }) => (
-    <div className={classNames("flex items-center px-8 pt-10 text-xs font-semibold uppercase text-gray-300")}>{n.text}</div>
+    <div className={classNames("flex items-center px-6 pt-10 text-xs font-semibold uppercase text-gray-300")}>{n.text}</div>
 );
