dc94cf0414a112ae5613d0e9f208cc9374504edb
Kuchasz <kuchasz1@o2.pl>
2023-10-29 13:15:29 +0100

    improved qr code generated file name

--- utils/src/file.ts
+++ utils/src/file.ts
@@ -5,25 +5,18 @@ export const triggerBase64Download = (base64Data: string, fileName: string) => {
     downloadLink.click();
 };
 
-const illegalRe = /[\/\?<>\\:\*\|"]/g;
+const illegalRe = /[\/\?<>\\:\*\,\|"]/g;
 const controlRe = /[\x00-\x1f\x80-\x9f]/g;
 const reservedRe = /^\.+$/;
 const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
-const windowsTrailingRe = /[\. ]+$/;
+const windowsTrailingRe = /[\. ]+$/g;
 
-const sanitize = (input: string, replacement: string) =>
+export const sanitizeFileName = (input: string, replacement = "") =>
     input
         .replace(illegalRe, replacement)
         .replace(controlRe, replacement)
         .replace(reservedRe, replacement)
         .replace(windowsReservedRe, replacement)
-        .replace(windowsTrailingRe, replacement);
-
-export const sanitizeFileName = (input: string) => {
-    const replacement = "";
-    const output = sanitize(input, replacement);
-    if (replacement === "") {
-        return output;
-    }
-    return sanitize(output, "");
-};
+        .replace(windowsTrailingRe, replacement)
+        .replace(/[ ]/g, "-")
+        .toLowerCase();
