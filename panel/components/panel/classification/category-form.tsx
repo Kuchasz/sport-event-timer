36cbed4d8242b5138cd2561cf2ae51997c1db721
Kuchasz <kuchasz1@o2.pl>
2023-10-29 23:38:24 +0100

    category form rewriten to form

--- panel/components/panel/classification/category-form.tsx
+++ panel/components/panel/classification/category-form.tsx
@@ -1,11 +1,12 @@
 import { Button } from "../../button";
-import { Label } from "../../label";
 import { PoorInput } from "../../poor-input";
-import { useFormState } from "hooks";
 import type { AppRouterInputs } from "trpc";
 import { PoorNumberInput } from "../../poor-number-input";
 import { RadioGroup } from "@headlessui/react";
 import classNames from "classnames";
+import { categorySchema } from "modules/classification/models";
+import { Form, FormInput } from "form";
+import { useTranslations } from "next-intl";
 
 type Category = AppRouterInputs["classification"]["addCategory"];
 
@@ -17,61 +18,90 @@ type CategoryFormProps = {
 };
 
 export const CategoryForm = ({ onReject, onResolve, initialCategory, isLoading }: CategoryFormProps) => {
-    const [category, changeHandler] = useFormState(initialCategory, [initialCategory]);
+    const t = useTranslations();
 
     return (
-        <div className="flex flex-col">
-            <div className="flex">
-                <div className="grow basis-full">
-                    <Label>Name</Label>
-                    <PoorInput value={category.name} onChange={changeHandler("name")}></PoorInput>
+        <Form<Category> initialValues={initialCategory} validationSchema={categorySchema} onSubmit={onResolve}>
+            <div className="flex flex-col">
+                <div className="flex">
+                    <FormInput<Category, "name">
+                        label={t("pages.classifications.categories.form.name.label")}
+                        render={({ value, onChange }) => (
+                            <PoorInput
+                                placeholder={t("pages.classifications.categories.form.name.placeholder")}
+                                value={value}
+                                onChange={onChange}
+                            />
+                        )}
+                        name="name"
+                    />
+                    <div className="p-2"></div>
+                    <FormInput<Category, "minAge">
+                        className="grow basis-full"
+                        label={t("pages.classifications.categories.form.minAge.label")}
+                        render={({ value, onChange }) => (
+                            <PoorNumberInput
+                                placeholder={t("pages.classifications.categories.form.minAge.placeholder")}
+                                value={value}
+                                onChange={onChange}
+                            />
+                        )}
+                        name="minAge"
+                    />
+                    <div className="p-2"></div>
+                    <FormInput<Category, "maxAge">
+                        className="grow basis-full"
+                        label={t("pages.classifications.categories.form.maxAge.label")}
+                        render={({ value, onChange }) => (
+                            <PoorNumberInput
+                                placeholder={t("pages.classifications.categories.form.maxAge.placeholder")}
+                                value={value}
+                                onChange={onChange}
+                            />
+                        )}
+                        name="maxAge"
+                    />
+                    <div className="p-2"></div>
+                    <FormInput<Category, "gender">
+                        className="grow basis-full"
+                        label={t("pages.classifications.categories.form.gender.label")}
+                        render={({ value, onChange }) => (
+                            <RadioGroup value={value} onChange={e => onChange({ target: { value: e } })}>
+                                <RadioGroup.Option value={null}>
+                                    {({ checked }) => (
+                                        <span className={classNames("cursor-pointer rounded-md p-1 text-xs", { ["bg-blue-200"]: checked })}>
+                                            {t("pages.classifications.categories.form.gender.kind.any")}
+                                        </span>
+                                    )}
+                                </RadioGroup.Option>
+                                <RadioGroup.Option value="male">
+                                    {({ checked }) => (
+                                        <span className={classNames("cursor-pointer rounded-md p-1 text-xs", { ["bg-blue-200"]: checked })}>
+                                            {t("pages.classifications.categories.form.gender.kind.male")}
+                                        </span>
+                                    )}
+                                </RadioGroup.Option>
+                                <RadioGroup.Option value="female">
+                                    {({ checked }) => (
+                                        <span className={classNames("cursor-pointer rounded-md p-1 text-xs", { ["bg-blue-200"]: checked })}>
+                                            {t("pages.classifications.categories.form.gender.kind.female")}
+                                        </span>
+                                    )}
+                                </RadioGroup.Option>
+                            </RadioGroup>
+                        )}
+                        name="gender"
+                    />
                 </div>
-                <div className="p-2"></div>
-                <div className="grow basis-full">
-                    <Label>Min Age</Label>
-                    <PoorNumberInput placeholder="Minimum Age" value={category.minAge} onChange={changeHandler("minAge")}></PoorNumberInput>
-                </div>
-                <div className="p-2"></div>
-                <div className="grow basis-full">
-                    <Label>Max Age</Label>
-                    <PoorNumberInput placeholder="Maximum Age" value={category.maxAge} onChange={changeHandler("maxAge")}></PoorNumberInput>
-                </div>
-                <div className="p-2"></div>
-                <div className="grow basis-full">
-                    <Label>Gender</Label>
-                    <RadioGroup value={category.gender} onChange={e => changeHandler("gender")({ target: { value: e } })}>
-                        <RadioGroup.Option value={null}>
-                            {({ checked }) => (
-                                <span className={classNames("cursor-pointer rounded-md p-1 text-xs", { ["bg-blue-200"]: checked })}>
-                                    Any
-                                </span>
-                            )}
-                        </RadioGroup.Option>
-                        <RadioGroup.Option value="male">
-                            {({ checked }) => (
-                                <span className={classNames("cursor-pointer rounded-md p-1 text-xs", { ["bg-blue-200"]: checked })}>
-                                    Male
-                                </span>
-                            )}
-                        </RadioGroup.Option>
-                        <RadioGroup.Option value="female">
-                            {({ checked }) => (
-                                <span className={classNames("cursor-pointer rounded-md p-1 text-xs", { ["bg-blue-200"]: checked })}>
-                                    Female
-                                </span>
-                            )}
-                        </RadioGroup.Option>
-                    </RadioGroup>
+                <div className="mt-4 flex justify-between">
+                    <Button onClick={onReject} outline>
+                        {t("shared.cancel")}
+                    </Button>
+                    <Button loading={isLoading} type="submit">
+                        {t("shared.save")}
+                    </Button>
                 </div>
             </div>
-            <div className="mt-4 flex justify-between">
-                <Button onClick={onReject} outline>
-                    Cancel
-                </Button>
-                <Button loading={isLoading} onClick={() => onResolve({ ...category })}>
-                    Save
-                </Button>
-            </div>
-        </div>
+        </Form>
     );
 };
--- panel/modules/classification/models.ts
+++ panel/modules/classification/models.ts
@@ -1,4 +1,15 @@
+import { genderEnum } from "../../models";
 import { z } from "zod";
+
+export const categorySchema = z.object({
+    id: z.number().min(1).optional(),
+    classificationId: z.number({ required_error: "classificationId is required" }).min(1),
+    name: z.string({ required_error: "name is required" }),
+    gender: genderEnum.nullish(),
+    minAge: z.number().min(1).nullish(),
+    maxAge: z.number().max(199).nullish(),
+    isSpecial: z.boolean(),
+});
 
 export const classificationSchema = z.object({
     id: z.number().nullish(),
