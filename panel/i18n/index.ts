cd7d0ac059098519aa9270343c1c77e2759cbcc7
Kuchasz <kuchasz1@o2.pl>
2023-10-30 18:48:39 +0100

    localized example data first step

--- panel/i18n/index.ts
+++ panel/i18n/index.ts
@@ -3,3 +3,5 @@ import { getRequestConfig } from "next-intl/server";
 export default getRequestConfig(async ({ locale }) => ({
     messages: (await import(`./resources/${locale}.json`)).default,
 }));
+
+export const locales = ["en", "pl"] as const;
