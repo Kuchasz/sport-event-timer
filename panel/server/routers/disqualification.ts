4981bf6ee7000e43212eae546b6a843d238fe3ca
Kuchasz <kuchasz1@o2.pl>
2023-11-02 00:15:15 +0100

    penalties and disqualification update added

--- panel/server/routers/disqualification.ts
+++ panel/server/routers/disqualification.ts
@@ -1,6 +1,13 @@
 import { protectedProcedure, router } from "../trpc";
 import { z } from "zod";
 
+const disqualificationSchema = z.object({
+    id: z.number(),
+    raceId: z.number(),
+    bibNumber: z.string(),
+    reason: z.string(),
+});
+
 export const disqualificationRouter = router({
     disqualifications: protectedProcedure
         .input(z.object({ raceId: z.number({ required_error: "raceId is required" }) }))
@@ -9,19 +16,16 @@ export const disqualificationRouter = router({
 
             return await ctx.db.disqualification.findMany({ where: { raceId } });
         }),
-    disqualify: protectedProcedure
-        .input(
-            z.object({
-                raceId: z.number(),
-                bibNumber: z.string(),
-                reason: z.string(),
-            }),
-        )
-        .mutation(async ({ input, ctx }) => {
-            const { ...timePenalty } = input;
+    disqualify: protectedProcedure.input(disqualificationSchema).mutation(async ({ input, ctx }) => {
+        const { ...disqualification } = input;
 
-            return await ctx.db.disqualification.create({ data: timePenalty });
-        }),
+        return await ctx.db.disqualification.create({ data: disqualification });
+    }),
+    update: protectedProcedure.input(disqualificationSchema).mutation(async ({ input, ctx }) => {
+        const { ...disqualification } = input;
+
+        return await ctx.db.disqualification.update({ where: { id: disqualification.id }, data: disqualification });
+    }),
     revert: protectedProcedure.input(z.object({ id: z.number() })).mutation(async ({ input, ctx }) => {
         const { id } = input;
 
