d90a8fa12601c6bcd31beca3aae41619448275ec
Kuchasz <kuchasz1@o2.pl>
2023-11-02 00:15:15 +0100

    penalties and disqualification update added

--- panel/server/routers/time-penalty.ts
+++ panel/server/routers/time-penalty.ts
@@ -1,6 +1,14 @@
 import { protectedProcedure, router } from "../trpc";
 import { z } from "zod";
 
+const timePenaltySchema = z.object({
+    id: z.number(),
+    raceId: z.number(),
+    bibNumber: z.string(),
+    time: z.number(),
+    reason: z.string(),
+});
+
 export const timePenaltyRouter = router({
     penalties: protectedProcedure
         .input(z.object({ raceId: z.number({ required_error: "raceId is required" }) }))
@@ -9,20 +17,16 @@ export const timePenaltyRouter = router({
 
             return await ctx.db.timePenalty.findMany({ where: { raceId } });
         }),
-    addPenalty: protectedProcedure
-        .input(
-            z.object({
-                raceId: z.number(),
-                bibNumber: z.string(),
-                time: z.number(),
-                reason: z.string(),
-            }),
-        )
-        .mutation(async ({ input, ctx }) => {
-            const { ...timePenalty } = input;
+    addPenalty: protectedProcedure.input(timePenaltySchema).mutation(async ({ input, ctx }) => {
+        const { ...timePenalty } = input;
 
-            return await ctx.db.timePenalty.create({ data: timePenalty });
-        }),
+        return await ctx.db.timePenalty.create({ data: timePenalty });
+    }),
+    update: protectedProcedure.input(timePenaltySchema).mutation(async ({ input, ctx }) => {
+        const { ...timePenalty } = input;
+
+        return await ctx.db.timePenalty.update({ where: { id: timePenalty.id }, data: timePenalty });
+    }),
     revert: protectedProcedure.input(z.object({ id: z.number() })).mutation(async ({ input, ctx }) => {
         const { id } = input;
 
