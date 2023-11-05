45f6dbe3f24046f21808ffc12bce65f0990da8d0
Kuchasz <kuchasz1@o2.pl>
2023-11-05 20:52:50 +0100

    paging added to data table

--- utils/src/number.ts
+++ utils/src/number.ts
@@ -3,3 +3,5 @@ export const inRange = (range: { from: number; to: number }, n: number) => range
 export const rangeLength = (range: { from: number; to: number }) => range.to - range.from + 1;
 
 export const formatNumber = (n: number, precision = 2) => n.toLocaleString("en-US", { minimumIntegerDigits: precision });
+
+export const clamp = (value: number, min: number, max: number) => Math.min(Math.max(value, min), max);
