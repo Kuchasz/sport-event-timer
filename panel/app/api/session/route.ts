db6b59d4ffa3bc556a14cd3e7ec1e9579fc24ecd
Kuchasz <kuchasz1@o2.pl>
2023-11-20 01:35:45 +0100

    login page translations added

--- panel/app/api/session/route.ts
+++ panel/app/api/session/route.ts
@@ -1,18 +1,15 @@
+import { notFound } from "next/navigation";
 import { type NextRequest, NextResponse } from "next/server";
 import { db } from "server/db";
 
 export async function POST(request: NextRequest) {
-    // const session = await getServerSession();
+    const { ip } = request;
+
+    if (ip) return notFound();
 
     const { sessionId } = await request.json();
-    // console.log("SESSSION_ID!!", sessionId);
-    // if (!session) {
-    //     return new NextResponse(JSON.stringify({ status: "fail", message: "You are not logged in" }), { status: 401 });
-    // }
 
-    const session = await db.session.findUnique({ where: { id: sessionId }, include: { user: true } }); //sessions[sessionId];
-
-    // return session?.valid ? session : null;
+    const session = await db.session.findUnique({ where: { id: sessionId }, include: { user: true } });
 
     return NextResponse.json({
         sessionId: session?.id,
