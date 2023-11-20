76422784bbbad9709f17a14e06503d2fa9362646
Kuchasz <kuchasz1@o2.pl>
2023-11-20 18:55:42 +0100

    registration init

--- panel/server/routers/user.ts
+++ panel/server/routers/user.ts
@@ -1,5 +1,7 @@
-import { loginSchema } from "modules/user/models";
+import { loginSchema, registrationSchema } from "modules/user/models";
 import { protectedProcedure, publicProcedure, router } from "../trpc";
+
+type ResultStatus = "Error" | "Success";
 
 export const userRouter = router({
     accounts: protectedProcedure.query(async ({ ctx }) => {
@@ -8,11 +10,20 @@ export const userRouter = router({
     login: publicProcedure.input(loginSchema).mutation(async ({ ctx, input }) => {
         const user = await ctx.db.user.findUnique({ where: { email: input.email } });
 
-        if (!user) return { status: "Error", message: "INVALID_USER_OR_PASSWORD" };
+        if (!user) return { status: "Error" as ResultStatus, message: "INVALID_USER_OR_PASSWORD" };
 
-        if (user.password === input.password) return { status: "Success" };
+        if (user.password === input.password) return { status: "Success" as ResultStatus };
 
-        return { status: "Error", message: "INVALID_USER_OR_PASSWORD" };
+        return { status: "Error" as ResultStatus, message: "INVALID_USER_OR_PASSWORD" };
+    }),
+    register: publicProcedure.input(registrationSchema).mutation(async ({ ctx, input }) => {
+        const user = await ctx.db.user.findUnique({ where: { email: input.email } });
+
+        if (!user) return { status: "Error" as ResultStatus, message: "INVALID_USER_OR_PASSWORD" };
+
+        if (user.password === input.password) return { status: "Success" as ResultStatus };
+
+        return { status: "Error" as ResultStatus, message: "INVALID_USER_OR_PASSWORD" };
     }),
 });
 
