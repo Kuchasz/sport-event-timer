d8fca2d4d48b45142c21ba74dadbf06026f4a217
Kuchasz <kuchasz1@o2.pl>
2023-11-30 22:58:02 +0100

    poor action component added

--- panel/components/poor-actions.tsx
+++ panel/components/poor-actions.tsx
@@ -12,6 +12,28 @@ type Action<TItem> = {
     execute: (param: TItem) => void;
 };
 
+type PoorActionProps = {
+    name: string;
+    description: string;
+    iconPath: string;
+};
+
+export const PoorAction = ({ name, description, iconPath }: PoorActionProps) => (
+    <a
+        key={name}
+        className="-m-3 flex cursor-pointer items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+        // onClick={() => action.execute(item)}
+    >
+        <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
+            <Icon size={1} className="text-black" path={iconPath} />,
+        </div>
+        <div className="ml-4">
+            <p className="text-sm font-medium text-gray-900">{name}</p>
+            <p className="text-sm text-gray-500">{description}</p>
+        </div>
+    </a>
+);
+
 export const PoorActions = <TItem,>({ item, actions }: { item: TItem; actions: Action<TItem>[] }) => {
     return (
         <Popover className="flex h-full items-center">
