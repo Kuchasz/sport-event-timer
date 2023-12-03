1015a78c3ace0ae121f209c84a19b499a09666d7
Kuchasz <kuchasz1@o2.pl>
2023-12-03 02:44:30 +0100

    noew modal

--- panel/components/modal.tsx
+++ panel/components/modal.tsx
@@ -2,13 +2,13 @@
 
 import { Dialog, Transition } from "@headlessui/react";
 import { Demodal, useModal } from "demodal";
-import type { ComponentType } from "react";
-import { Fragment, useState } from "react";
+import type { FunctionComponent } from "react";
+import React, { Fragment, useState } from "react";
 import { Button } from "./button";
 import { useTranslations } from "next-intl";
 
 type ResolvableComponentProps<P> = { onResolve: (value: P) => void; onReject: () => void };
-type ResolvableComponent<P> = ComponentType<ResolvableComponentProps<P>>;
+type ResolvableComponent<P> = FunctionComponent<ResolvableComponentProps<P>>;
 type ResolvableModalProps<P> = {
     title: string;
     description?: string;
@@ -183,6 +183,64 @@ export const ConfirmationModal = ({ onAccept, title, description, message, child
                                                 </Button>
                                             </div>
                                         </div>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition>
+        </>
+    );
+};
+
+type ResolvableModalComponentProps<P> = { onResolve: (value: P) => void };
+type ResolvableModalComponent<T, P> = FunctionComponent<T & ResolvableModalComponentProps<P>>;
+
+type ModalProps<T, P> = {
+    title: string;
+    description?: string;
+    onResolve: (data: P) => void;
+    children: React.ReactElement;
+    component: ResolvableModalComponent<T, P>;
+    componentProps: T;
+};
+
+export const ModalModal = <T, P>({ onResolve, title, description, children, component: Content, componentProps }: ModalProps<T, P>) => {
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+    return (
+        <>
+            <span onClick={() => setModalOpen(!modalOpen)}>{children}</span>
+            <Transition appear show={modalOpen} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={() => setModalOpen(false)}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0">
+                        <div className="fixed inset-0 bg-black bg-opacity-25" />
+                    </Transition.Child>
+                    <div className="fixed inset-0 overflow-y-auto">
+                        <div className="flex min-h-full items-center justify-center p-4 text-center">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95">
+                                <Dialog.Panel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                    <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                                        {title}
+                                    </Dialog.Title>
+                                    <Dialog.Description>{description}</Dialog.Description>
+                                    <div className="mt-2">
+                                        <Content {...componentProps} onResolve={onResolve} />
                                     </div>
                                 </Dialog.Panel>
                             </Transition.Child>
