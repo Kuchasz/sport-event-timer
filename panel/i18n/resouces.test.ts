51360f95f6bf3446b3634a449aab17855b39a22e
Kuchasz <kuchasz1@o2.pl>
2023-12-11 02:13:30 +0100

    test improved

--- panel/i18n/resouces.test.ts
+++ panel/i18n/resouces.test.ts
@@ -1,18 +1,14 @@
 import { excludeItems } from "@set/utils/dist/array";
 import { getAllPropertyNames } from "@set/utils/dist/object";
 import { errorKeys } from "../modules/shared/error-keys";
-import enTranslations from "./resources/en.json";
-import plTranslations from "./resources/pl.json";
+import { locales } from "./index";
 
 describe("Errors translations", () => {
     const errorTranslationKeys = Object.values(errorKeys) as string[];
 
-    test("Polish error translations", () => {
-        const notTranslatedErrorKeys = excludeItems(errorTranslationKeys, getAllPropertyNames(plTranslations));
-        expect(notTranslatedErrorKeys).toHaveLength(0);
-    });
-    test("English error translations", () => {
-        const notTranslatedErrorKeys = excludeItems(errorTranslationKeys, getAllPropertyNames(enTranslations));
+    test.each(locales)("%s error translations", async locale => {
+        const translations = await import(`./resources/${locale}.json`);
+        const notTranslatedErrorKeys = excludeItems(errorTranslationKeys, getAllPropertyNames(translations));
         expect(notTranslatedErrorKeys).toHaveLength(0);
     });
 });
