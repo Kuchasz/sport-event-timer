7376e62821e918d227491ebf63ff7c83552d040f
Kuchasz <kuchasz1@o2.pl>
2023-12-11 01:23:17 +0100

    improvement objects

--- panel/i18n/resouces.test.ts
+++ panel/i18n/resouces.test.ts
@@ -1,41 +1,19 @@
 import enTranslations from "./resources/en.json";
 import plTranslations from "./resources/pl.json";
 import { errorKeys } from "../modules/shared/error-keys";
+import { createNestedObject, getObjectSlice } from "@set/utils/dist/object";
 
 describe("Errors translations", () => {
     //eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    const rootKeys = [...new Set<string>(Object.values(errorKeys).map(k => k.split(".")[0]))];
     const allValues = createNestedObject(Object.values(errorKeys).map(k => [k, expect.any(String)]));
+    const slicedPlTranslations = getObjectSlice(plTranslations, rootKeys);
+    const slicedEnTranslations = getObjectSlice(enTranslations, rootKeys);
 
     test("Polish error translations", () => {
-        expect(plTranslations).toMatchObject(expect.objectContaining(allValues));
+        expect(slicedPlTranslations).toMatchObject(expect.objectContaining(allValues));
     });
     test("English error translations", () => {
-        expect(enTranslations).toMatchObject(expect.objectContaining(allValues));
+        expect(slicedEnTranslations).toMatchObject(expect.objectContaining(allValues));
     });
 });
-
-function createNestedObject(entries: [string, string][]): Record<string, any> {
-    const result: Record<string, any> = {};
-
-    entries.forEach(([path, value]) => {
-        const keys: string[] = path.split(".");
-        let currentLevel: Record<string, any> = result;
-
-        keys.forEach((key, index) => {
-            if (!currentLevel[key]) {
-                if (index === keys.length - 1) {
-                    // Last key, assign the value
-                    currentLevel[key] = value;
-                } else {
-                    // Create an empty object for the next level
-                    currentLevel[key] = {};
-                }
-            }
-
-            // Move to the next level
-            currentLevel = currentLevel[key];
-        });
-    });
-
-    return result;
-}
