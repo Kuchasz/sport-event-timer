346529dafe11772c3f64442b99b4d574740249b3
Kuchasz <kuchasz1@o2.pl>
2023-12-14 23:44:32 +0100

    disqualifications added to time penalties

--- panel/server/routers/disqualification.ts
+++ panel/server/routers/disqualification.ts
@@ -3,6 +3,18 @@ import { protectedProcedure, router } from "../trpc";
 import { z } from "zod";
 
 export const disqualificationRouter = router({
+    allDisqualifications: protectedProcedure
+        .input(z.object({ raceId: z.number({ required_error: "raceId is required" }) }))
+        .query(async ({ input, ctx }) => {
+            const raceId = input.raceId;
+            const players = await ctx.db.player.findMany({ where: { raceId }, include: { profile: true } });
+            const playersByBibNumbers = new Map<string, { name: string; lastName: string }>(
+                players.map(p => [p.bibNumber, { name: p.profile.name, lastName: p.profile.lastName }]),
+            );
+            const disqualifications = await ctx.db.disqualification.findMany({ where: { raceId } });
+
+            return disqualifications.map(p => ({ ...p, player: playersByBibNumbers.get(p.bibNumber) }));
+        }),
     disqualifications: protectedProcedure
         .input(z.object({ raceId: z.number({ required_error: "raceId is required" }) }))
         .query(async ({ input, ctx }) => {
