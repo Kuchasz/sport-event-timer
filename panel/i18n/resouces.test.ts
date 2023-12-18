4666ac093c7a7640a8817741dbc792c717b9c1f4
Kuchasz <kuchasz1@o2.pl>
2023-12-18 21:53:53 +0100

    sorted translations + added test for them

--- panel/i18n/resouces.test.ts
+++ panel/i18n/resouces.test.ts
@@ -5,10 +5,31 @@ import { locales } from "./index";
 
 describe("Errors translations", () => {
     const errorTranslationKeys = Object.values(errorKeys);
+    const nonEnglishLocales = excludeItems(locales as unknown as string[], ["en"]);
 
     test.each(locales)("%s error translations", async locale => {
         const translations = await import(`./resources/${locale}.json`);
         const notTranslatedErrorKeys = excludeItems(errorTranslationKeys, getAllPropertyNames(translations));
         expect(notTranslatedErrorKeys).toHaveLength(0);
     });
+
+    test.each(nonEnglishLocales)("%s missing translations", async locale => {
+        const translations = await import(`./resources/${locale}.json`);
+        const englishTranslations = await import("./resources/en.json");
+
+        const translationsEquality = hasEqualStructure(translations, englishTranslations);
+
+        expect(translationsEquality).toBeTruthy();
+    });
 });
+
+const hasEqualStructure = (obj1: Record<string, unknown>, obj2: Record<string, unknown>): boolean =>
+    Object.keys(obj1).every(key => {
+        const v = obj1[key];
+
+        if (typeof v === "object" && v !== null) {
+            return hasEqualStructure(v as Record<string, unknown>, obj2[key] as Record<string, unknown>);
+        }
+
+        return obj2.hasOwnProperty(key);
+    });
