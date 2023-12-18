ffe8fa70007a751361bc37e03f75b10d9ff6087b
Kuchasz <kuchasz1@o2.pl>
2023-12-18 22:36:32 +0100

    poor select 2 removed

--- panel/components/poor-select.tsx
+++ panel/components/poor-select.tsx
@@ -1,7 +1,7 @@
+import { Listbox, Transition } from "@headlessui/react";
+import { mdiArrowUpDown, mdiCheck } from "@mdi/js";
 import Icon from "@mdi/react";
 import { Fragment, useState } from "react";
-import { Listbox, Transition } from "@headlessui/react";
-import { mdiArrowUpDown, mdiCheck, mdiChevronDown } from "@mdi/js";
 
 export const PoorSelect = <T, TNameKey extends keyof T, TValueKey extends keyof T>({
     items,
@@ -71,80 +71,5 @@ export const PoorSelect = <T, TNameKey extends keyof T, TValueKey extends keyof 
                 </Transition>
             </div>
         </Listbox>
-    );
-};
-
-export const PoorSelect2 = <T, TNameKey extends keyof T, TValueKey extends keyof T>({
-    items,
-    initialValue,
-    nameKey,
-    valueKey,
-    onChange,
-    placeholder,
-}: {
-    initialValue?: T[TValueKey];
-    items: T[];
-    nameKey: TNameKey;
-    valueKey: TValueKey;
-    onChange: (event: { target: { value: T[TValueKey] } }) => void;
-    placeholder?: string;
-}) => {
-    const [selected, setSelected] = useState(initialValue);
-
-    const desiredItem =
-        selected && items?.length > 0
-            ? items.find(i => String(i[valueKey]) === String(selected))!
-            : initialValue && items?.length > 0
-            ? items.find(i => String(i[valueKey]) === String(initialValue))!
-            : ({ [valueKey]: -1, [nameKey]: placeholder || "Select value" } as unknown as T);
-
-    return (
-        <div className="min-w-[14rem]">
-            <Listbox
-                value={selected}
-                onChange={(e: T[TValueKey]) => {
-                    setSelected(e);
-                    const desiredItem = items.find(i => String(i[valueKey]) === String(e))!;
-                    onChange({ target: { value: desiredItem[valueKey] } });
-                }}>
-                <div className="relative w-full flex-shrink px-2">
-                    <Listbox.Button className="relative w-full cursor-pointer rounded-lg bg-gray-100 py-2 pl-3 pr-10 text-left hover:bg-gray-200 focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                        <span className="block truncate">{String(desiredItem[nameKey])}</span>
-                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                            {/* <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" /> */}
-                            <Icon className="text-gray-400" path={mdiChevronDown} size={0.8} />
-                        </span>
-                    </Listbox.Button>
-                    <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
-                        <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                            {items.map((item, personIdx) => (
-                                <Listbox.Option
-                                    key={personIdx}
-                                    className={({ active }) =>
-                                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                            active ? "bg-amber-100 text-amber-900" : "text-gray-900"
-                                        }`
-                                    }
-                                    value={item[valueKey]}>
-                                    {({ selected }) => (
-                                        <>
-                                            <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
-                                                {String(item[nameKey])}
-                                            </span>
-                                            {selected ? (
-                                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                                                    {/* <CheckIcon className="h-5 w-5" aria-hidden="true" /> */}
-                                                    <Icon path={mdiCheck} size={0.8} />
-                                                </span>
-                                            ) : null}
-                                        </>
-                                    )}
-                                </Listbox.Option>
-                            ))}
-                        </Listbox.Options>
-                    </Transition>
-                </div>
-            </Listbox>
-        </div>
     );
 };
--- panel/components/status-components.tsx
+++ panel/components/status-components.tsx
@@ -2,42 +2,8 @@
 
 import { mdiPowerStandby } from "@mdi/js";
 import Icon from "@mdi/react";
-import { useCurrentRaceId } from "hooks";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import type { AppRouterOutputs } from "trpc";
 import { trpc } from "trpc-core";
-import { PoorSelect2 } from "./poor-select";
-
-type Races = AppRouterOutputs["race"]["myRaces"];
-
-export const RaceSelector = ({ races: initialData }: { races: Races }) => {
-    const { data: items } = trpc.race.myRaces.useQuery(undefined, { initialData });
-    const [selectedRaceId, selectRaceId] = useState<number>(0);
-    const raceId = useCurrentRaceId() || selectedRaceId;
-    const router = useRouter();
-
-    // console.log(selectedRaceId);
-
-    useEffect(() => {
-        !selectedRaceId && items?.length && selectRaceId(items[0]?.id);
-    }, [items]);
-
-    return (
-        <PoorSelect2
-            placeholder="Select race"
-            initialValue={raceId}
-            onChange={e => {
-                if (!e.target.value) return;
-                selectRaceId(e.target.value);
-                router.push(`/${e.target.value}`);
-            }}
-            valueKey="id"
-            nameKey="name"
-            items={items}
-        />
-    );
-};
 
 export const LogoutButton = () => {
     const logoutMutation = trpc.user.logout.useMutation();
