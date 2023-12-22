5f7f89e1f2bb2872755a1e86363687b3d3518e98
Kuchasz <kuchasz1@o2.pl>
2023-12-23 00:27:40 +0100

    applied first toasts for actions

--- panel/components/toaster.tsx
+++ panel/components/toaster.tsx
@@ -1,19 +1,39 @@
 "use client";
 
-import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "./toast";
+import { mdiAlertCircleOutline, mdiAlertOutline, mdiCheckCircleOutline, mdiInformationSlabCircleOutline } from "@mdi/js";
+import Icon from "@mdi/react";
+import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport, getColorFromVariant } from "./toast";
 import { useToast } from "./use-toast";
+import classNames from "classnames";
+
+const getIconFromVariant = (variant: "default" | "destructive" | "dangerous" | "positive") => {
+    if (variant === "default") return mdiInformationSlabCircleOutline;
+    if (variant === "destructive") return mdiAlertCircleOutline;
+    if (variant === "dangerous") return mdiAlertOutline;
+    return mdiCheckCircleOutline;
+};
 
 export function Toaster() {
     const { toasts } = useToast();
 
     return (
-        <ToastProvider>
+        <ToastProvider duration={4000}>
             {toasts.map(function ({ id, title, description, action, ...props }) {
                 return (
                     <Toast key={id} {...props}>
-                        <div className="grid gap-1">
-                            {title && <ToastTitle>{title}</ToastTitle>}
-                            {description && <ToastDescription>{description}</ToastDescription>}
+                        <div className="grid gap-1" style={{ gridTemplateColumns: "auto minmax(0, 1fr)" }}>
+                            <span className="row-span-2 mr-4 flex items-center">
+                                <span className={classNames("rounded-md p-1", getColorFromVariant(props.variant ?? "default"))}>
+                                    <Icon
+                                        className="box-border text-white"
+                                        path={getIconFromVariant(props.variant ?? "default")}
+                                        size={1}></Icon>
+                                </span>
+                            </span>
+                            <span className="col-start-2 row-start-1">{title && <ToastTitle>{title}</ToastTitle>}</span>
+                            <span className="col-start-2 row-start-2">
+                                {description && <ToastDescription>{description}</ToastDescription>}
+                            </span>
                         </div>
                         {action}
                         <ToastClose />
