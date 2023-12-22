6f6951cf7a705fc0c31b522500edafc3f041205d
Kuchasz <kuchasz1@o2.pl>
2023-12-22 12:41:10 +0100

    timing points actions improvements

--- panel/app/[locale]/(panel)/[raceId]/timing-points/[timingPointId]/timing-point.tsx
+++ panel/app/[locale]/(panel)/[raceId]/timing-points/[timingPointId]/timing-point.tsx
@@ -13,6 +13,8 @@ import Head from "next/head";
 import type { AppRouterOutputs } from "trpc";
 import { useCurrentRaceId } from "../../../../../../hooks";
 import { trpc } from "../../../../../../trpc-core";
+import { Button } from "components/button";
+import { NewPoorActionsItem, PoorActions } from "components/poor-actions";
 
 type TimingPoint = AppRouterOutputs["timingPoint"]["timingPoints"][0];
 type AccessKeys = AppRouterOutputs["timingPoint"]["timingPointAccessUrls"];
@@ -31,13 +33,19 @@ const generateAccessUrl = async () => {
     });
 };
 
-export const TimingPoint = ({ timingPoint }: { timingPoint: TimingPoint }) => {
+export const TimingPoint = ({ initialTimingPoint }: { initialTimingPoint: TimingPoint }) => {
     const raceId = useCurrentRaceId();
 
     const { data: accessKeys, refetch: refetchAccessKeys } = trpc.timingPoint.timingPointAccessUrls.useQuery(
-        { raceId: raceId, timingPointId: timingPoint.id },
+        { raceId: raceId, timingPointId: initialTimingPoint.id },
         { initialData: [] },
     );
+
+    const { data: timingPoint, refetch: refetchTimingPoint } = trpc.timingPoint.timingPoint.useQuery(
+        { raceId: raceId, timingPointId: initialTimingPoint.id },
+        { initialData: initialTimingPoint },
+    );
+
     const t = useTranslations();
 
     const deleteTimingPointMutation = trpc.timingPoint.delete.useMutation();
@@ -51,40 +59,34 @@ export const TimingPoint = ({ timingPoint }: { timingPoint: TimingPoint }) => {
             sortable: false,
             cellRenderer: d => d.expireDate.toLocaleString(),
         },
-        { field: "code", headerName: t("pages.timingPoints.accessUrls.grid.columns.code"), sortable: true },
         {
             field: "canAccessOthers",
             headerName: t("pages.timingPoints.accessUrls.grid.columns.supervisor"),
             sortable: true,
             cellRenderer: d => (d.canAccessOthers ? "true" : "false"),
         },
-        { field: "token", headerName: t("pages.timingPoints.accessUrls.grid.columns.token"), sortable: true },
         {
             field: "id",
-            headerName: t("pages.timingPoints.accessUrls.grid.columns.url"),
+            headerName: t("pages.timingPoints.accessUrls.grid.columns.actions"),
             sortable: false,
             cellRenderer: d => (
-                <div>
-                    <span>http://app.url/{d.token}</span>
-                    <button onClick={generateAccessUrl} className="hover:text-blue-600">
-                        <Icon className="ml-2" path={mdiClipboardFileOutline} size={0.8}></Icon>
-                    </button>
-                </div>
-            ),
-        },
-        {
-            field: "id",
-            headerName: t("pages.timingPoints.accessUrls.edit.button"),
-            sortable: false,
-            cellRenderer: d => (
-                <PoorConfirmation
-                    onAccept={() => deleteAccessKey(d)}
-                    title={t("pages.timingPoints.accessUrls.delete.confirmation.title")}
-                    message={t("pages.timingPoints.accessUrls.delete.confirmation.text", { name: d.name })}>
-                    <button className="font-medium hover:text-red-600 hover:underline">
-                        <Icon path={mdiTrashCanOutline} size={0.8}></Icon>
-                    </button>
-                </PoorConfirmation>
+                <PoorActions>
+                    <PoorConfirmation
+                        onAccept={() => deleteAccessKey(d)}
+                        title={t("pages.timingPoints.accessUrls.delete.confirmation.title")}
+                        message={t("pages.timingPoints.accessUrls.delete.confirmation.text", { name: d.name })}>
+                        <NewPoorActionsItem
+                            name={t("pages.timingPoints.accessUrls.delete.title")}
+                            description={t("pages.timingPoints.accessUrls.delete.description")}
+                            iconPath={mdiTrashCanOutline}></NewPoorActionsItem>
+                    </PoorConfirmation>
+
+                    <NewPoorActionsItem
+                        onClick={generateAccessUrl}
+                        name={t("pages.timingPoints.accessUrls.copy.title")}
+                        description={t("pages.timingPoints.accessUrls.copy.description")}
+                        iconPath={mdiClipboardFileOutline}></NewPoorActionsItem>
+                </PoorActions>
             ),
         },
     ];
@@ -105,10 +107,10 @@ export const TimingPoint = ({ timingPoint }: { timingPoint: TimingPoint }) => {
     return (
         <>
             <Head>
-                <title>{t("pages.timingPoints.header.title")}</title>
+                <title>{t("pages.timingPoint.header.title")}</title>
             </Head>
             <div className="border-1 flex h-full flex-col border-solid border-gray-600">
-                <PageHeader title={t("pages.timingPoints.header.title")} description={t("pages.timingPoints.header.description")} />
+                <PageHeader title={t("pages.timingPoint.header.title")} description={t("pages.timingPoint.header.description")} />
                 <div className="flex">
                     <div className="mt-1 w-full flex-grow">
                         <div className="flex flex-grow rounded-lg bg-gray-50 p-6">
@@ -118,7 +120,7 @@ export const TimingPoint = ({ timingPoint }: { timingPoint: TimingPoint }) => {
                             </div>
                             <div className="flex items-center">
                                 <PoorModal
-                                    onResolve={() => {}}
+                                    onResolve={() => refetchTimingPoint()}
                                     title={t("pages.timingPoints.edit.title")}
                                     component={TimingPointEdit}
                                     componentProps={{
@@ -141,25 +143,27 @@ export const TimingPoint = ({ timingPoint }: { timingPoint: TimingPoint }) => {
                         </div>
                         <div className="mt-8">
                             <div className="flex items-center">
-                                <div>
-                                    <h3 className="text-xl font-semibold">{t("pages.timingPoints.accessUrls.header.title")}</h3>
-                                    <div>{t("pages.timingPoints.accessUrls.header.description")}</div>
-                                </div>
-                                <div className="flex-grow"></div>
-                                <PoorModal
-                                    onResolve={() => refetchAccessKeys()}
-                                    title={t("pages.timingPoints.accessUrls.create.title")}
-                                    component={TimingPointAccessUrlCreate}
-                                    componentProps={{
-                                        timingPointId: timingPoint.id,
-                                        raceId: timingPoint.raceId,
-                                        onReject: () => {},
-                                    }}>
-                                    <button className="rounded-lg bg-gray-100 p-3 text-gray-600 hover:bg-gray-200">
-                                        <Icon path={mdiPlus} size={0.8}></Icon>
-                                    </button>
-                                </PoorModal>
+                                <PageHeader
+                                    title={t("pages.timingPoints.accessUrls.header.title")}
+                                    description={t("pages.timingPoints.accessUrls.header.description")}
+                                />
                             </div>
+                            <div className="p-2"></div>
+                            <PoorModal
+                                onResolve={() => refetchAccessKeys()}
+                                title={t("pages.timingPoints.accessUrls.create.title")}
+                                component={TimingPointAccessUrlCreate}
+                                componentProps={{
+                                    timingPointId: timingPoint.id,
+                                    raceId: timingPoint.raceId,
+                                    onReject: () => {},
+                                }}>
+                                <Button outline>
+                                    <Icon size={0.8} path={mdiPlus} />
+                                    <span className="ml-2">{t("pages.timingPoints.accessUrls.create.button")}</span>
+                                </Button>
+                            </PoorModal>
+                            <div className="p-2"></div>
                             <PoorDataTable columns={cols} getRowId={d => d.id} gridName="timing-point-access-keys" data={accessKeys} />
                         </div>
                     </div>
