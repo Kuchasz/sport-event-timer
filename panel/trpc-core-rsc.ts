1743b3b0e9b00a86c2bf76cdb6a55d536ad7398f
Kuchasz <kuchasz1@o2.pl>
2023-12-29 19:40:00 +0100

    fixed api port in trpc rsc

--- panel/trpc-core-rsc.ts
+++ panel/trpc-core-rsc.ts
@@ -4,7 +4,7 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import { env } from "env";
 import { headers } from "next/headers";
 
-const url = env.NEXT_PUBLIC_NODE_ENV === "production" ? `https://${env.NEXT_PUBLIC_APP_URL}` : `http://${env.NEXT_PUBLIC_APP_URL}`;
+const url = `http://localhost:${env.API_PORT}`;
 
 export const trpcRSC = createTRPCProxyClient<AppRouter>({
     transformer: superjson,
