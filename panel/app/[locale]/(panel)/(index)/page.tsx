8d6433701d0a8c66332023b0cf367661a9d33add
Kuchasz <kuchasz1@o2.pl>
2023-12-29 16:59:58 +0100

    test

--- panel/app/[locale]/(panel)/(index)/page.tsx
+++ panel/app/[locale]/(panel)/(index)/page.tsx
@@ -1,10 +1,10 @@
 import { authenticate } from "auth";
 import { Races } from "./races";
-import { trpcRSC } from "trpc-core-rsc";
+// import { trpcRSC } from "trpc-core-rsc";
 
 export default async function () {
     await authenticate();
-    const races = await trpcRSC.race.races.query();
+    const races = [] as any; //await trpcRSC.race.races.query();
 
     return <Races initialData={races} />;
 }
