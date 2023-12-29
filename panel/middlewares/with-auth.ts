ab3801be01d16a0ede0537e39bc5b012a8110207
Kuchasz <kuchasz1@o2.pl>
2023-12-29 20:19:09 +0100

    domain added to cookies

--- panel/middlewares/with-auth.ts
+++ panel/middlewares/with-auth.ts
@@ -1,6 +1,7 @@
 import { type NextFetchEvent, type NextRequest, NextResponse } from "next/server";
 import type { MiddlewareFactory } from "./stack-handler";
 import { getUserSession } from "auth";
+import { env } from "env";
 
 export const withAuth: MiddlewareFactory = next => async (request: NextRequest, _next: NextFetchEvent) => {
     if (!/((api).*)/.test(request.nextUrl.pathname.slice(1))) {
@@ -14,10 +15,13 @@ export const withAuth: MiddlewareFactory = next => async (request: NextRequest, 
 
         if (response instanceof NextResponse) {
             if (session.accessToken) {
+                const domain = env.NEXT_PUBLIC_NODE_ENV === "production" ? "rura.cc;" : "";
                 response.cookies.set("accessToken", session.accessToken, {
                     httpOnly: true,
                     secure: true,
                     maxAge: 15,
+                    sameSite: "none",
+                    domain: domain,
                     path: "/",
                 });
             }
