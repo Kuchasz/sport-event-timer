1e09e2ffcb1f114d96506016812425c2a4df49d0
Kuchasz <kuchasz1@o2.pl>
2023-12-30 01:17:34 +0100

    margin fix

--- panel/components/timer/clock.tsx
+++ panel/components/timer/clock.tsx
@@ -1,23 +1,38 @@
 import { formatTimeWithMilliSec, formatTimeWithSec } from "@set/utils/dist/datetime";
+import { type CSSProperties } from "react";
 
-const Time = ({ className, time, fontSize, full = false }: { className?: string; time: number; fontSize?: number; full?: boolean }) => (
+const Time = ({
+    style,
+    className,
+    time,
+    fontSize,
+    full = false,
+}: {
+    style?: CSSProperties;
+    className?: string;
+    time: number;
+    fontSize?: number;
+    full?: boolean;
+}) => (
     <div
-        style={{ fontSize: fontSize ? `${fontSize}rem` : undefined }}
+        style={{ fontSize: fontSize ? `${fontSize}rem` : undefined, ...style }}
         className={`${className} self-center p-2 text-center font-mono leading-none transition-all`}>
         {full ? formatTimeWithMilliSec(time) : formatTimeWithSec(time)}
     </div>
 );
 
 export const Clock = ({
+    style,
     className,
     time,
     fontSize,
     full = false,
 }: {
+    style?: CSSProperties;
     className?: string;
     time: number;
     fontSize?: number;
     full?: boolean;
 }) => {
-    return <Time className={className} time={time} fontSize={fontSize} full={full} />;
+    return <Time style={style} className={className} time={time} fontSize={fontSize} full={full} />;
 };
