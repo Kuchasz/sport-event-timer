91e880fe263fd23f4ef363f9f2f78bf6f1712c36
Kuchasz <kuchasz1@o2.pl>
2024-01-04 02:38:45 +0100

    still wip

--- panel/trpc-core-rsc.ts
+++ panel/trpc-core-rsc.ts
@@ -1,7 +1,7 @@
 import superjson from "superjson";
 import { type AppRouter } from "./server/routers/app";
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
-import { env } from "env";
+import { env } from "./env";
 import { headers } from "next/headers";
 
 const url = `http://localhost:${env.API_PORT}`;
