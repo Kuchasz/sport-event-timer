e3399dd1aaf1bfc003375daede30f5c5bb12211b
Kuchasz <kuchasz1@o2.pl>
2024-01-08 23:29:08 +0100

    config page improvements

--- panel/app/[locale]/(stopwatch)/stopwatch/[raceId]/config/config.tsx
+++ panel/app/[locale]/(stopwatch)/stopwatch/[raceId]/config/config.tsx
@@ -8,10 +8,13 @@ import Icon from "@mdi/react";
 import { mdiCheck } from "@mdi/js";
 import classNames from "classnames";
 import { trpc } from "trpc-core";
+import { useTranslations } from "next-intl";
 
 export const Config = () => {
     const [timingPointId, chooseTimingPoint] = useAtom(timingPointIdAtom);
     const { raceId } = useParams<{ raceId: string }>()!;
+
+    const t = useTranslations();
 
     const { data: allTimingPoints } = trpc.timingPoint.timingPoints.useQuery(
         { raceId: parseInt(raceId) },
@@ -35,19 +38,36 @@ export const Config = () => {
 
     return (
         <div className="flex h-full w-full flex-col items-center">
-            <div className="flex h-full w-full flex-grow flex-col items-center justify-center">
-                {sortedTimingPoints.map((tk, id) => (
+            <div className="flex h-full w-full flex-grow flex-col justify-center px-4">
+                <div className="font-semibold">{t("stopwatch.config.selectTimingPoint.header")}</div>
+                <div className="text-xs">{t("stopwatch.config.selectTimingPoint.description")}</div>
+                {sortedTimingPoints.map((timingPoint, id) => (
                     <button
-                        onClick={() => setTimingPointId(tk.id)}
-                        className="relative my-2 flex items-center rounded-xl bg-white px-3 py-2 shadow transition-opacity"
-                        key={tk.id}>
+                        onClick={() => setTimingPointId(timingPoint.id)}
+                        className="relative my-2 flex w-full items-center px-3 py-2"
+                        key={timingPoint.id}>
                         <TimingPointIcon isFirst={id === 0} isLast={id === sortedTimingPoints.length - 1} />
-                        <span className="ml-4 text-xl">{tk.name}</span>
-                        <Icon
-                            className={classNames("transition-all", { ["ml-3"]: tk.id === timingPointId })}
-                            size={tk.id === timingPointId ? 1 : 0}
+                        <div className="mx-4 flex flex-grow flex-col items-start">
+                            <div className="font-semibold">{timingPoint.name}</div>
+                            <div className="text-xs opacity-75">{timingPoint.description}</div>
+                        </div>
+                        <div className="flex h-6 w-6 items-center justify-center">
+                            <div
+                                className={classNames("flex h-4 w-4 items-center justify-center rounded-full bg-gray-300 transition-all", {
+                                    ["h-6 w-6 bg-orange-500"]: timingPoint.id === timingPointId,
+                                })}>
+                                <Icon
+                                    className={classNames("text-white transition-all")}
+                                    size={timingPoint.id === timingPointId ? 0.5 : 0}
+                                    path={mdiCheck}
+                                />
+                            </div>
+                        </div>
+                        {/* <Icon
+                            className={classNames("transition-all", { ["ml-3"]: timingPoint.id === timingPointId })}
+                            size={timingPoint.id === timingPointId ? 1 : 0}
                             path={mdiCheck}
-                        />
+                        /> */}
                     </button>
                 ))}
             </div>
