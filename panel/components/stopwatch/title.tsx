874f897416d9b605b974a745914ad4aee912c8e2
Kuchasz <kuchasz1@o2.pl>
2024-01-17 01:00:36 +0100

    small title change

--- panel/components/stopwatch/title.tsx
+++ panel/components/stopwatch/title.tsx
@@ -7,11 +7,11 @@ import { useRouter } from "next/navigation";
 export const Title = ({ text }: { text: string }) => {
     const router = useRouter() as unknown as { back: () => void };
     return (
-        <div className="relative z-10 flex w-full items-center bg-white px-1 py-2 shadow-sm will-change-transform">
+        <div className="relative z-10 flex w-full items-center bg-white px-1 py-3 shadow-sm will-change-transform">
             <div onClick={router.back} className="absolute">
                 <Icon size={1.2} path={mdiChevronLeft}></Icon>
             </div>
-            <div className="w-full text-center font-semibold">{text}</div>
+            <div className="w-full text-center text-sm font-medium text-black">{text}</div>
         </div>
     );
 };
