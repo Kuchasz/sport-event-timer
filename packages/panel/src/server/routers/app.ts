1c6410823f8b25fdf913aee1dac4982eb46c945b
Kuchasz <kuchasz1@o2.pl>
2024-02-25 20:12:46 +0100

    turned off not needed routes

--- packages/panel/src/server/routers/app.ts
+++ packages/panel/src/server/routers/app.ts
@@ -1,34 +1,21 @@
 import { router } from "../trpc";
-import { actionRouter } from "./action";
-import { apiKeyRouter } from "./api-key";
-import { classificationRouter } from "./classification";
-import { ntpRouter } from "./ntp";
-import { playerRouter } from "./player";
-import { raceRouter } from "./race";
 import { resultRouter } from "./result";
-import { splitTimeRouter } from "./split-time";
-import { timingPointRouter } from "./timing-point";
-import { playerRegistrationRouter } from "./player-registration";
-import { userRouter } from "./user";
-import { bibNumberRouter } from "./bib-number";
-import { timePenaltyRouter } from "./time-penalty";
-import { disqualificationRouter } from "./disqualification";
 
 export const appRouter = router({
-    classification: classificationRouter,
-    player: playerRouter,
-    race: raceRouter,
-    timingPoint: timingPointRouter,
-    action: actionRouter,
-    ntp: ntpRouter,
-    splitTime: splitTimeRouter,
+    // classification: classificationRouter,
+    // player: playerRouter,
+    // race: raceRouter,
+    // timingPoint: timingPointRouter,
+    // action: actionRouter,
+    // ntp: ntpRouter,
+    // splitTime: splitTimeRouter,
     result: resultRouter,
-    apiKey: apiKeyRouter,
-    playerRegistration: playerRegistrationRouter,
-    user: userRouter,
-    bibNumber: bibNumberRouter,
-    timePenalty: timePenaltyRouter,
-    disqualification: disqualificationRouter,
+    // apiKey: apiKeyRouter,
+    // playerRegistration: playerRegistrationRouter,
+    // user: userRouter,
+    // bibNumber: bibNumberRouter,
+    // timePenalty: timePenaltyRouter,
+    // disqualification: disqualificationRouter,
 });
 
 // export type definition of API
