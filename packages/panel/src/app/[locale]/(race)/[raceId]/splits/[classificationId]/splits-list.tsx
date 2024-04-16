5c6bc9362068cdb4060eb4c53156f1384e259854
Kuchasz <kuchasz1@o2.pl>
2024-04-17 00:21:27 +0200

    huge table changes

--- packages/panel/src/app/[locale]/(race)/[raceId]/splits/[classificationId]/splits-list.tsx
+++ packages/panel/src/app/[locale]/(race)/[raceId]/splits/[classificationId]/splits-list.tsx
@@ -1,47 +1,37 @@
 "use client";
 
-import { mdiDrag, mdiPlus, mdiTrashCanOutline } from "@mdi/js";
+import { mdiArrowRight, mdiDrag, mdiPlus, mdiTrashCanOutline } from "@mdi/js";
 import Icon from "@mdi/react";
 import classNames from "classnames";
 import { useTranslations } from "next-intl";
 import React, { useState } from "react";
 import { Button } from "src/components/button";
 import { SectionHeader } from "src/components/page-headers";
-import { PoorDataTable, PoorDataTableColumn } from "src/components/poor-data-table";
 import { PoorInput } from "src/components/poor-input";
 import { PoorSelect } from "src/components/poor-select";
-import { Label } from "src/form";
 import type { AppRouterOutputs } from "src/trpc";
 import { trpc } from "src/trpc-core";
 
 type TimingPoint = AppRouterOutputs["timingPoint"]["timingPoints"][0];
 type Split = AppRouterOutputs["split"]["splits"][0];
 
-const DropTarget = ({ onDrop, moveMode }: { onDrop: () => void; moveMode: number }) => (
-    <div
-        onClick={onDrop}
-        className={classNames(
-            "text-2xs col-span-5 flex cursor-pointer justify-center overflow-hidden rounded-md bg-gray-100 font-semibold uppercase text-gray-700 transition-all hover:bg-gray-50",
-            moveMode ? "h-auto px-2 py-1" : "h-0",
-        )}>
-        drop element here
-    </div>
-);
-
-const SplitButton = ({ onClick, icon }: { onClick: () => void; icon: string }) => {
+const SplitButton = ({ className, onClick, icon }: { className?: string; onClick: () => void; icon: string }) => {
     return (
-        <div className="flex cursor-pointer items-center self-center rounded-full p-2 hover:bg-gray-100" onClick={onClick}>
+        <div
+            className={classNames("flex cursor-pointer items-center self-center rounded-full p-2 hover:bg-gray-100", className)}
+            onClick={onClick}>
             <Icon size={0.8} path={icon} />
         </div>
     );
 };
 
-const SplitRow = ({
+const TableSplitRow = ({
     split,
     onSplitChange,
     timingPoints,
     moveMode,
     onEnableMoveMode,
+    onTriggerMove,
     onDeleteSplit,
 }: {
     split: Split;
@@ -49,26 +39,44 @@ const SplitRow = ({
     timingPoints: TimingPoint[];
     moveMode: number;
     onEnableMoveMode: (id: number) => void;
+    onTriggerMove: (id: number) => void;
     onDeleteSplit: (id: number) => void;
 }) => {
     return (
-        <div
-            className={classNames(
-                "col-span-5 grid grid-cols-subgrid transition-opacity",
-                moveMode && moveMode !== split.id && "pointer-events-none opacity-25",
-            )}>
-            <SplitButton onClick={() => onEnableMoveMode(split.id)} icon={mdiDrag} />
-            <PoorInput value={split.name} onChange={e => onSplitChange({ ...split, name: e.target.value })}></PoorInput>
-            <PoorSelect
-                initialValue={split.timingPointId}
-                items={timingPoints}
-                placeholder="choose timing point"
-                nameKey="name"
-                valueKey="id"
-                onChange={e => onSplitChange({ ...split, timingPointId: e.target.value })}></PoorSelect>
-            <PoorInput value={split.name} onChange={e => onSplitChange({ ...split, name: e.target.value })}></PoorInput>
-            <SplitButton onClick={() => onDeleteSplit(split.id)} icon={mdiTrashCanOutline} />
-        </div>
+        // <div
+        //     className={classNames(
+        //         "col-span-5 grid grid-cols-subgrid transition-opacity",
+        //         moveMode && moveMode !== split.id && "pointer-events-none opacity-25",
+        //     )}>
+        <tr className={classNames("border-b")}>
+            {/* <div className="absolute top-0 flex h-full items-center bg-red-500">awdawdaadawdadwadwadwawd</div> */}
+            <td className="relative p-2">
+                {moveMode && moveMode !== split.id ? (
+                    <SplitButton onClick={() => onTriggerMove(split.id)} icon={mdiArrowRight} />
+                ) : (
+                    <SplitButton onClick={() => onEnableMoveMode(split.id)} icon={mdiDrag} />
+                )}
+            </td>
+            <td className={classNames("p-2 transition-opacity", moveMode && moveMode !== split.id && "pointer-events-none opacity-15")}>
+                <PoorInput value={split.name} onChange={e => onSplitChange({ ...split, name: e.target.value })}></PoorInput>
+            </td>
+            <td className={classNames("p-2 transition-opacity", moveMode && moveMode !== split.id && "pointer-events-none opacity-15")}>
+                <PoorSelect
+                    initialValue={split.timingPointId}
+                    items={timingPoints}
+                    placeholder="choose timing point"
+                    nameKey="name"
+                    valueKey="id"
+                    onChange={e => onSplitChange({ ...split, timingPointId: e.target.value })}></PoorSelect>
+            </td>
+            <td className={classNames("p-2 transition-opacity", moveMode && moveMode !== split.id && "pointer-events-none opacity-15")}>
+                <PoorInput value={split.name} onChange={e => onSplitChange({ ...split, name: e.target.value })}></PoorInput>
+            </td>
+            <td className={classNames("p-2 transition-opacity", moveMode && moveMode !== split.id && "pointer-events-none opacity-15")}>
+                <SplitButton onClick={() => onDeleteSplit(split.id)} icon={mdiTrashCanOutline} />
+            </td>
+        </tr>
+        // </div>
     );
 };
 
@@ -80,6 +88,10 @@ type SplitsProps = {
     splits: Split[];
     splitsOrder: number[];
 };
+
+const PoorTableHeader = ({ children }: { children: React.ReactNode }) => (
+    <th className="m-2 select-none whitespace-nowrap rounded-md bg-white px-2 py-3">{children}</th>
+);
 
 export const SplitsList = ({ timingPoints, classificationId, classificationName, raceId, splits, splitsOrder }: SplitsProps) => {
     const [newSplits, setNewSplits] = useState<Split[]>(splits);
@@ -133,6 +145,13 @@ export const SplitsList = ({ timingPoints, classificationId, classificationName,
         setNewSplitsOrder(newOrder);
     };
 
+    const handleTriggerMove = (id: number) => {
+        const splitId = moveMode;
+        const dropIndex = newSplitsOrder.indexOf(id);
+
+        moveSplitToIndex(dropIndex, splitId);
+    };
+
     const handleSplitChange = (split: Split) => {
         const index = newSplits.findIndex(s => s.id === split.id);
         const newSplitsCopy = [...newSplits];
@@ -140,64 +159,47 @@ export const SplitsList = ({ timingPoints, classificationId, classificationName,
         setNewSplits(newSplitsCopy);
     };
 
-    const cols: PoorDataTableColumn<Split>[] = [
-        { field: "name", headerName: "Name", sortable: false },
-        {
-            field: "timingPointId",
-            headerName: "TimingPoint",
-            sortable: false,
-        },
-        {
-            field: "name",
-            headerName: "Distance",
-            sortable: false,
-        },
-        {
-            field: "name",
-            allowShrink: true,
-            headerName: "",
-            sortable: false,
-            cellRenderer: () => <SplitButton onClick={() => {}} icon={mdiTrashCanOutline} />,
-        },
-    ];
-
     return (
         <div>
             <SectionHeader title={classificationName} />
-
-            <PoorDataTable<Split>
-                hideColumnsChooser
-                hidePaging
-                gridName="time-penalties"
-                columns={cols}
-                data={splitsInOrder ?? []}
-                getRowId={p => p.id}></PoorDataTable>
-
-            <div className="mt-4 grid gap-2" style={{ gridTemplateColumns: "min-content 1fr 1fr 1fr min-content" }}>
-                <div></div>
-                <Label>Name</Label>
-                <Label>Timing Point</Label>
-                <Label>Distance</Label>
-                <Label></Label>
-                {splitsInOrder.map((s, index) => (
-                    <React.Fragment key={s.id}>
-                        <DropTarget onDrop={() => moveSplitToIndex(index, moveMode)} moveMode={moveMode} />
-                        <SplitRow
-                            split={s}
-                            onSplitChange={handleSplitChange}
-                            timingPoints={timingPoints}
-                            moveMode={moveMode}
-                            onEnableMoveMode={handleMoveMode}
-                            onDeleteSplit={handleDeleteSplit}
-                        />
-                        {index === splitsInOrder.length - 1 && (
-                            <DropTarget onDrop={() => moveSplitToIndex(index + 1, moveMode)} moveMode={moveMode} />
-                        )}
-                    </React.Fragment>
-                ))}
-                <div className="col-span-4 flex items-center justify-end"></div>
-                <SplitButton icon={mdiPlus} onClick={addSplit} />
+            <div className="my-8 border">
+                <table className="min-w-full bg-white">
+                    <thead className="text-left text-xs">
+                        <tr className="border-b">
+                            <PoorTableHeader> </PoorTableHeader>
+                            <PoorTableHeader>Name</PoorTableHeader>
+                            <PoorTableHeader>Timing Point</PoorTableHeader>
+                            <PoorTableHeader>Distance</PoorTableHeader>
+                            <PoorTableHeader> </PoorTableHeader>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td colSpan={5} className="table-cell justify-end pb-2 pt-4">
+                                <div className="flex w-full items-center justify-end">
+                                    <div onClick={addSplit} className="flex cursor-pointer items-center justify-end pr-3 hover:underline">
+                                        <Icon path={mdiPlus} size={0.8}></Icon>
+                                        <span className="ml-1 text-sm">Add split</span>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        {splitsInOrder.map(s => (
+                            <TableSplitRow
+                                key={s.id}
+                                split={s}
+                                onSplitChange={handleSplitChange}
+                                timingPoints={timingPoints}
+                                moveMode={moveMode}
+                                onTriggerMove={handleTriggerMove}
+                                onEnableMoveMode={handleMoveMode}
+                                onDeleteSplit={handleDeleteSplit}
+                            />
+                        ))}
+                    </tbody>
+                </table>
             </div>
+
             <div className="mt-4 flex">
                 <Button outline>{t("shared.cancel")}</Button>
                 <Button onClick={handleSaveChanges} loading={updateSplitsMutation.isLoading} className="ml-2" type="submit">
